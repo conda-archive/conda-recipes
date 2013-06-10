37bfcf4c2ac485421f2b41550487692ca37fcf4a
Aaron Meurer <asmeurer@gmail.com>
2013-06-10 14:06:57 -0500

    Add qt-4.8.4 from Anaconda

---
+++ qt-4.8.4/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+$REPLACE "read acceptance" "acceptance=yes" configure
+$REPLACE "read commercial" "commercial=o" configure
+
+if [ `uname` == Darwin ]; then
+    patch -p0 <$PKG_PATH/10.5.patch || exit 1
+    chmod +x configure
+
+    ./configure \
+        -platform macx-g++ \
+        -release -no-qt3support -nomake examples -nomake demos \
+        -opensource \
+        -no-framework \
+        -sdk $SDK \
+        -arch $OSX_ARCH \
+        -prefix $PREFIX
+
+    for x in $(find . -name Makefile); do
+      $REPLACE "-arch -Wl,-syslibroot,/Developer/SDKs/MacOSX10.5.sdk" "" $x
+    done
+fi
+
+if [ `uname` == Linux ]; then
+    chmod +x configure
+    ./configure \
+        -release -fontconfig -continue -verbose \
+        -no-qt3support -nomake examples -nomake demos \
+        -qt-libpng -qt-zlib \
+        -prefix $PREFIX
+fi
+
+make || exit 1
+make install
+
+if [ `uname` == Darwin ]; then
+    cd $PREFIX
+    rm -rf doc imports mkspecs doc plugins phrasebooks translations \
+        q3porting.xml
+    cd bin
+    rm -rf *.app qcollectiongenerator qhelpgenerator qt3to4 qdoc3
+    rm -rf xml* rcc uic moc macdeployqt
+fi
+
+if [ `uname` == Linux ]; then
+    cp $SRC_DIR/bin/* $PREFIX/bin/
+    cd $PREFIX
+    rm -rf doc imports mkspecs phrasebooks plugins q3porting.xml translations
+    cd $PREFIX/bin
+    rm -f *.bat *.pl qt3to4 qdoc3
+fi
