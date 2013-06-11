7354e874af987e31c982401d24c09d1fe3dec955
Aaron Meurer <asmeurer@gmail.com>
2013-06-11 18:46:53 -0500

    Make things work in windows and unux

--- python-patch/run_test.py
+++ python-patch/run_test.py
@@ -1,6 +1,13 @@
 import subprocess
 
-subprocess.check_call(["patch.bat", "testfile.patch"])
+import sys
+
+if sys.platform == 'win32':
+    command = 'patch.bat'
+else:
+    command = 'patch.py'
+
+subprocess.check_call([command, "testfile.patch"])
 with open("testfile") as f1:
     testfile1 = f1.read()
 with open("testfile2") as f2:
