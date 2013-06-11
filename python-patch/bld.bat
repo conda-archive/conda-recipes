95d9de2cdec30ca65415d14940c4055fbd1d9505
Aaron Meurer <asmeurer@gmail.com>
2013-06-11 18:20:58 -0500

    Write a bld.bat for python-patch

--- python-patch/bld.bat
+++ python-patch/bld.bat
@@ -1,8 +1,2 @@
-python setup.py install
+copy %RECIPE_DIR%\patch.py %PREFIX%\scripts\patch.py
 if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: https://github.com/ContinuumIO/conda/blob/master/conda/builder/README.txt
-:: for a list of environment variables that are set during the build process.
