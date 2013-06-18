f390b1e21446a981b06b47e52c485e82c208362d
Aaron Meurer <asmeurer@gmail.com>
2013-06-18 10:26:53 -0500

    Add keyring and appdirs (dependencies of binstar client)

---
+++ appdirs/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PYTHON setup.py install || exit 1
+
+# Add more build steps here, if they are necessary.
+
+# See
+# https://github.com/ContinuumIO/conda/blob/master/conda/builder/README.txt
+# for a list of environment variables that are set during the build process.
