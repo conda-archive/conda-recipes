3660d0464df9125b2df211025477219764dabae6
Aaron Meurer <asmeurer@gmail.com>
2013-06-21 14:27:10 -0500

    Hard code the version

--- ipython/bld.bat
+++ ipython/bld.bat
@@ -4,4 +4,4 @@ if errorlevel 1 exit 1
 python setup.py install
 if errorlevel 1 exit 1
 
-rd /s /q %SP_DIR%\ipython-%PKG_VERSION%-py%PY_VER%.egg\share
+rd /s /q %SP_DIR%\ipython-0.13.2-py%PY_VER%.egg\share
