713c125c5ddfc72dce3439f730444f0336e9bcd2
Ilan Schnell <ilanschnell@gmail.com>
2013-06-29 10:40:52 -0500

    add trilinos recipe

---
+++ trilinos/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+cmake \
+  -D CMAKE_BUILD_TYPE:STRING=RELEASE \
+  -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
+  -D DART_TESTING_TIMEOUT:STRING=600 \
+  -D Trilinos_ENABLE_Fortran:BOOL=OFF \
+  -D Trilinos_ENABLE_TESTS:BOOL=OFF \
+  -D Trilinos_ENABLE_DEBUG:BOOL=OFF \
+  -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
+  -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON \
+  -D Trilinos_ENABLE_PyTrilinos:BOOL=ON \
+  -D BUILD_SHARED_LIBS:BOOL=ON \
+  -D SWIG_EXECUTABLE:FILEPATH=$PREFIX/bin/swig \
+  -D CMAKE_INSTALL_PREFIX=$PREFIX \
+  ..
+
+make
+make install
