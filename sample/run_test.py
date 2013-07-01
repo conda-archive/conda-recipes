30e479f1abb63b37def693484066187ccecccb6f
Ilan Schnell <ilanschnell@gmail.com>
2013-07-01 16:01:35 -0500

    add debug output

--- sample/run_test.py
+++ sample/run_test.py
@@ -7,4 +7,5 @@ assert test_pycosat.run().wasSuccessful()
 if sys.platform != 'win32':
     assert pycosat.__version__ == '0.6.0'
 
+sys.stdout.write("The answer is: %d\n" % test_pycosat.foo)
 assert test_pycosat.foo == 42
