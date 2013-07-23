70e4585073970adc8175a6ff26221a0ffe1ead66
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2013-07-23 11:15:34 +0200

    Fixed link to conda builder docs, closes #6 Use the docs.continuum.io link

--- milk/build.sh
+++ milk/build.sh
@@ -6,5 +6,5 @@ $PYTHON setup.py install
 # Add more build steps here, if they are necessary.
 
 # See
-# https://github.com/ContinuumIO/conda/blob/master/conda/builder/README.txt
+# http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
