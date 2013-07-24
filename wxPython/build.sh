0699dd6924d003ff5af16745e31b40683b06dcd2
Aaron Meurer <asmeurer@gmail.com>
2013-07-24 10:29:53 -0500

    First shot at wxPython

---
+++ wxPython/build.sh
@@ -0,0 +1,24 @@
+mkdir bld
+cd bld
+../configure --prefix=$PREFIX \
+    --with-mac \
+    --with-opengl \
+    --enable-geometry \
+    --enable-graphics_ctx \
+    --enable-sound --with-sdl \
+    --enable-mediactrl \
+    --enable-display \
+    --enable-monolithic
+
+make \
+    && make -C contrib/src/gizmos \
+                && make -C contrib/src/stc
+
+make install \
+    && make -C contrib/src/gizmos install \
+    && make -C contrib/src/stc install
+
+export WX_CONFIG=$PREFIX/bin/wx-config
+
+cd ../wxPython
+python setup.py build_ext --inplace
