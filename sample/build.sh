f6d9f9371ea3870199057122cf7d3b2d2bf327bf
Ilan Schnell <ilanschnell@gmail.com>
2013-08-28 00:49:10 -0500

    add post-link.sh for testing

--- sample/build.sh
+++ sample/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 $PYTHON setup.py install
+
+POST_LINK=$PREFIX/bin/.pycosat-post-link.sh
+cp $RECIPE_DIR/post-link.sh $POST_LINK
+chmod +x $POST_LINK
---
+++ sample/post-link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# we have to append
+cat <<EOF >>${PREFIX}/.link-messages.txt
+This is an install message from the sample pycosat package
+EOF
