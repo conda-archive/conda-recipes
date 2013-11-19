45a6eced3109cdfdfc79225d65e7a905bf866d72
KTK <ktk227@gmail.com>
2013-11-19 11:45:25 -0500

    Can build with Anaconda Boost if installed Require libxml2 To import XdmfCore do import Xdmf.Core To import XdmfUtils do import Xdmf.Utils

--- xdmf2/build.sh
+++ xdmf2/build.sh
@@ -11,7 +11,11 @@ if [ `uname` == Darwin ]; then
 fi
 
 # Need boost includes >= 1.41.0
-export BOOST_ROOT=/usr
+if [[ -d $SYS_PREFIX/include/boost ]]; then
+    BOOST="-DBoost_NO_BOOST_CMAKE:BOOL=ON -DBoost_INCLUDE_DIR:PATH=$SYS_PREFIX/include -DBoost_LIBRARY_DIRS:FILEPATH=$SYS_PREFIX/lib"
+else
+    BOOST="-DBoost_DIR=/usr"
+fi
 
 mkdir build
 cd build
@@ -23,6 +27,7 @@ cmake                                                                      \
     -DPYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python${PY_VER}               \
     -DPYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER}             \
     -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.so            \
+    $BOOST                                                                 \
     -DXDMF_BUILD_TESTING:BOOL=ON                                           \
     -DXDMF_BUILD_UTILS:BOOL=ON                                             \
     -DXDMF_WRAP_PYTHON:BOOL=ON                                             \
@@ -35,6 +40,8 @@ make install
 cat > $PREFIX/lib/python/__init__.py << __EOF__
 
 from Xdmf import *
+import XdmfCore as Core
+import XdmfUtils as Utils
 
 __version__ = "20131030"
 
