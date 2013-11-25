a44b0dacbc328ae179c62782c87d2b447189b3a0
Christopher Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2013-11-24 22:21:45 -0600

    Removed copiler flags from build.sh

--- pymc/build.sh
+++ pymc/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export CFLAGS="-arch x86_64"
-export FFLAGS="-static -ff2c -arch x86_64"
-export LDFLAGS="-Wall -undefined dynamic_lookup -bundle -arch x86_64"
 
+$PYTHON setup.py build
 $PYTHON setup.py install
-
