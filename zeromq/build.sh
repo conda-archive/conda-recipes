bebbc7eebe22fc345223bb6eef8c73d41acc76bd
MinRK <benjaminrk@gmail.com>
2013-11-25 15:37:54 -0800

    update zeromq to 4.0.3
    and link libsodium

--- zeromq/build.sh
+++ zeromq/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # default is 10.5 for some reason, which doesn't work
 export MACOSX_DEPLOYMENT_TARGET=10.6
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --with-libsodium=$PREFIX
 make -j
 make install
