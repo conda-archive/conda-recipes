68fd3e9902d40c49b3e5ab84f6068be9bacfffd8
Min RK <benjaminrk@gmail.com>
2013-11-25 17:43:45 -0800

    link zeromq in pyzmq on Windows

--- pyzmq/bld.bat
+++ pyzmq/bld.bat
@@ -1,11 +1,7 @@
+set DISTUTILS_USE_SDK=1
+
 "%PYTHON%" setup.py configure --zmq "%PREFIX%"
 if errorlevel 1 exit 1
 
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
