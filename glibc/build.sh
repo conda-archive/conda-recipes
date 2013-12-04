23568d63e70d8a6aada43607482893e6a150cd69
Dan Blanchard <dblanchard@ets.org>
2013-12-04 16:56:20 -0500

    Another attempt at getting this to work

--- glibc/build.sh
+++ glibc/build.sh
@@ -4,10 +4,11 @@ cd build
 export CC=cc
 export CXX=c++
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
-export CFLAGS="-m64 -pipe -O2 -march=core2"
+export CFLAGS="-pipe -O2"
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
-../configure --prefix=$PREFIX
+../configure --prefix=$PREFIX --enable-hardcoded-path-in-test
 make
+make check
 make install
