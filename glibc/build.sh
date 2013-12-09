99b7a1f3f598e569ac222bd50e463b82d1f8a5b6
Dan Blanchard <dblanchard@ets.org>
2013-12-09 15:37:57 -0500

    Try to simplify build.sh and see if that fixes things.

--- glibc/build.sh
+++ glibc/build.sh
@@ -2,9 +2,7 @@ mkdir build
 cd build
 
 export CC=gcc
-export CFLAGS="-pipe -O2"
-export LDFLAGS="-L$PREFIX/lib"
-../configure --prefix=$PREFIX --enable-hardcoded-path-in-test --enable-kernel-version=2.6.32
+../configure --prefix=$PREFIX --enable-kernel-version=2.6.32
 make -j 2
 make check
 make install
