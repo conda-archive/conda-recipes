aaf3e0fc685c076f8193cd49fea78c36395e2877
irritum <kamil.kwiek@continuum.io>
2013-12-10 18:53:47 +0100

    Added recipe for pysam 0.7.7 package

---
+++ pysam/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build dependencies:
+# - samtools-libs
+# - samtools-devel
+
+export CFLAGS="-m64 -pipe -O2 -march=x86-64"
+export CXXFLAGS="${CFLAGS}"
+
+mkdir -vp ${PREFIX}/bin;
+
+touch requirements.txt;
+
+${PYTHON} setup.py install || exit 1;
+
+POST_LINK="${PREFIX}/bin/.pysam-post-link.sh"
+cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
+chmod -v 0755 ${POST_LINK};
