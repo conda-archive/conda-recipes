9d50ab3f0dfdaabcfa6df9958db1244494034d25
irritum <kamil.kwiek@continuum.io>
2013-12-11 16:55:43 +0100

    Added recipe for ilmbase 2.1.0 package (not working yet)
    There is a problem with compilation on centos machine:
    configure: error: POSIX thread support required
    
    This is bizzare since necessary files exist:
    /lib64/libpthread-2.12.so
    /lib64/libpthread.so.0
    /usr/include/pthread.h
    /usr/include/bits/pthreadtypes.h
    /usr/lib64/libpthread.so
    /usr/lib64/libpthread_nonshared.a
    /usr/lib64/pkgconfig/pthread-stubs.pc

---
+++ ilmbase/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir -vp ${PREFIX}/bin;
+
+MACHINE="$(uname 2>/dev/null)"
+
+export CFLAGS="-m64 -pipe -O2 -march=x64-64 -fPIC"
+export CXXLAGS="${CFLAGS}"
+#export CPPFLAGS="-I${PREFIX}/include"
+#export LDFLAGS="-L${PREFIX}/lib64"
+
+LinuxInstallation() {
+
+    chmod +x configure;
+
+    ./configure \
+        --prefix ${PREFIX} || return 1;
+    make || return 1;
+    make install || return 1;
+
+    return 0;
+}
+
+case ${MACHINE} in
+    'Linux')
+        LinuxInstallation || exit 1;
+        ;;
+    *)
+        echo -e "Unsupported machine type: ${MACHINE}";
+        exit 1;
+        ;;
+esac
+
+POST_LINK="${PREFIX}/bin/.ilmbase-post-link.sh"
+cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
+chmod -v 0755 ${POST_LINK};
