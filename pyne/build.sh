7efb256b559e46ce1015da71705f0e8105f65cb3
Cameron Bates <cameron.r.bates@gmail.com>
2013-12-10 20:37:36 -0800

    Update recipe for pyne to work with staging

--- pyne/build.sh
+++ pyne/build.sh
@@ -2,5 +2,9 @@
 
 $PYTHON setup.py install
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+cp build/pyne/lib/*.dylib $PREFIX/lib
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
 find . -name '*.so' | xargs chmod +x
-cp build/cpp/*.so $PREFIX/lib
+cp build/pyne/lib/*.so $PREFIX/lib
+fi
