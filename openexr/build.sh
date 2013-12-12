7fd4a7d5a278d69a3f46284114b67bbc3210e41e
irritum <kamil.kwiek@continuum.io>
2013-12-12 11:10:18 +0100

    Upgraded recipe for openexr to 2.0.1 version (not working yet)

--- openexr/build.sh
+++ openexr/build.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-# Build dependencies:
-# - OpenEXR-libs
-# - OpenEXR-devel
-
-export CFLAGS="-m64 -pipe -O2 -march=x86-64"
-export CXXFLAGS="${CFLAGS}"
-
-touch requirements.txt;
-
 mkdir -vp ${PREFIX}/bin;
 
-${PYTHON} setup.py install || exit 1;
+MACHINE="$(uname 2>/dev/null)"
+
+export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+LinuxInstallation() {
+
+    chmod +x configure;
+
+    ./configure \
+        --prefix ${PREFIX} || return 1;
+    make || return 1;
+    make install || return 1;
+
+    return 0;
+}
+
+case ${MACHINE} in
+    'Linux')
+        LinuxInstallation || exit 1;
+        ;;
+    *)
+        echo -e "Unsupported machine type: ${MACHINE}";
+        exit 1;
+        ;;
+esac
 
 POST_LINK="${PREFIX}/bin/.openexr-post-link.sh"
 cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
 chmod -v 0755 ${POST_LINK};
+
