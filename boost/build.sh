591f0b1daf7d04f2b2b8c96c25829e42271bb98e
irritum <kamil.kwiek@continuum.io>
2013-12-12 10:35:29 +0100

    Updated recipe for boost 1.55.0

--- boost/build.sh
+++ boost/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Hints:
+# http://boost.2283326.n4.nabble.com/how-to-build-boost-with-bzip2-in-non-standard-location-td2661155.html
+# http://www.gentoo.org/proj/en/base/amd64/howtos/?part=1&chap=3
+
 # Build dependencies:
 # - bzip2-devel
 
-export CFLAGS="-m64 -pipe -O2 -march=x86-64"
+export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC -shared"
 export CXXFLAGS="${CFLAGS}"
 
 mkdir -vp ${PREFIX}/bin;
