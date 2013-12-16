2a0e1869ee38a13f54349011d59123f2cacbdeee
irritum <kamil.kwiek@continuum.io>
2013-12-16 10:06:42 +0100

    Added recipe for hdf4 4.2.9 package

---
+++ hdf4/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+mkdir -vp ${PREFIX}/bin;
+
+MACHINE="$(uname 2>/dev/null)"
+
+export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
+export CXXFLAGS="${CFLAGS}"
+#export CPPFLAGS="-I${PREFIX}/include"
+#export LDFLAGS="-L${PREFIX}/lib"
+
+LinuxInstallation() {
+
+    chmod +x configure;
+
+    ./configure \
+        --disable-static \
+        --enable-linux-lfs \
+        --with-ssl \
+        --with-zlib \
+        --prefix ${PREFIX} || return 1;
+    make || return 1;
+    make install || return 1;
+
+    rm -rf ${PREFIX}/share/hdf4_examples;
+
+    return 0;
+}
+
+case ${MACHINE} in
+    'Linux')
+        LinuxInstallation || exit 1;
+        ;;
+    *)
+        echo -e "Unsupported machine type: ${MACHINE}";
+        exit 1;
+        ;;
+esac
+
+POST_LINK="${PREFIX}/bin/.hdf4-post-link.sh"
+cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
+chmod -v 0755 ${POST_LINK};
+
