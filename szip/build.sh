4a689b70a3b00743bd1f4c41c06dae74b3bfb6bb
irritum <kamil.kwiek@continuum.io>
2013-12-16 10:38:40 +0100

    Added recipe for szip 2.1 package

---
+++ szip/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mkdir -vp ${PREFIX}/bin;
+
+MACHINE="$(uname 2>/dev/null)"
+
+export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
+export CXXLAGS="${CFLAGS}"
+#export CPPFLAGS="-I${PREFIX}/include"
+#export LDFLAGS="-L${PREFIX}/lib64"
+
+LinuxInstallation() {
+
+    chmod +x configure;
+
+    ./configure \
+        --disable-encoding \
+        --prefix ${PREFIX} || return 1;
+    make || return 1;
+    make install || return 1;
+
+    return 0;
+}
+
+case ${MACHINE} in
+    'Linux')
+        LinuxInstallation || exit 1;
+        ;;
+    *)
+        echo -e "Unsupported machine type: ${MACHINE}";
+        exit 1;
+        ;;
+esac
+
+POST_LINK="${PREFIX}/bin/.szip-post-link.sh"
+cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
+chmod -v 0755 ${POST_LINK};
