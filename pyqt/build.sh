42f516c2216bcd04afe5032050cb1b97ee6d4bab
Aaron Meurer <asmeurer@gmail.com>
2014-01-21 10:11:02 -0600

    Some more attempts at pyqt

--- pyqt/build.sh
+++ pyqt/build.sh
@@ -1,4 +1,13 @@
-$PYTHON configure-ng.py --verbose
+export QMAKESPEC="unsupported/macx-clang-libc++"
+$PYTHON -c "import os;print(os.environ)"
+$PYTHON configure.py --verbose \
+        --confirm-license \
+        --bindir=$PREFIX/bin \
+        --destdir=$SP_DIR \
+
+
+
+$PYTHON configure-ng.py --verbose --spec="unsupported/macx-clang-libc++"
 
 make
 make install
