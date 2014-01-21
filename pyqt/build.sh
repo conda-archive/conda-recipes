d8d1b8850aa6710e039a19453bad08607d2797d7
Aaron Meurer <aaron.meurer@continuum.io>
2014-01-21 16:05:50 -0600

    Fix pyqt build

--- pyqt/build.sh
+++ pyqt/build.sh
@@ -8,16 +8,17 @@ fi
 sudo mkdir -p /opt/anaconda1anaconda2anaconda3/
 sudo chmod 777 /opt/anaconda1anaconda2anaconda3
 ln -s $PREFIX/mkspecs /opt/anaconda1anaconda2anaconda3/mkspecs 
+ln -s $PREFIX/include /opt/anaconda1anaconda2anaconda3/include
+ln -s $PREFIX/lib /opt/anaconda1anaconda2anaconda3/lib
+ln -s $PREFIX/bin /opt/anaconda1anaconda2anaconda3/bin
 
-$PYTHON configure.py --verbose \
+$PYTHON configure-ng.py --verbose \
         --confirm-license \
         --bindir=$PREFIX/bin \
         --destdir=$SP_DIR \
-
-
 
 # $PYTHON configure-ng.py --verbose --spec="unsupported/macx-clang-libc++"
 
 make
 make install
-exit 1
+
