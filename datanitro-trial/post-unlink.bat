c5d28efa35979f1aa2bc6835ac160b181bef4f8a
Trent Nelson <trent@trent.me>
2014-01-26 02:11:29 -0500

    Implement uninstaller functionality.

--- datanitro-trial/post-unlink.bat
+++ datanitro-trial/post-unlink.bat
@@ -1,3 +1,13 @@
 
+set SCRIPTS=%~dp0
+
+for /f %%i in ("%SCRIPTS%..") do (
+    set CONDA_ROOT=%%~fi
+)
+
+set DN=%CONDA_ROOT%\datanitro-trial
+set UNINSTALL_EXE=%DN%\uninstaller.exe
+
+start /wait /min cmd /c %UNINSTALL_EXE% /S
 
 rem vim:set ts=8 sw=4 sts=4 tw=78 et:
