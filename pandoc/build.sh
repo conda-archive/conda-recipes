2f1cf7ad28efc4c95d0fec8a05f16fa300900764
Aaron Meurer <asmeurer@gmail.com>
2014-01-29 11:15:34 -0600

    Hopefully adding ~/.cabal/bin to the PATH will fix the problem

--- pandoc/build.sh
+++ pandoc/build.sh
@@ -1,3 +1,10 @@
+# This might be different in general. You can find the general location by
+# running ghci and running
+#   :m System.Directory
+#   getAppUserDataDirectory "cabal"
+# I couldn't figure out how to run this dynamically. ghci doesn't seem to have
+# a -c switch like python.
+export PATH="$HOME/.cabal/bin:$PATH"
 cabal install alex happy
 cabal install --only-dependencies
 cabal install hsb2hs  # a required build tool
