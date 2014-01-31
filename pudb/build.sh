6c2c4bcadd055edca57d3350c8d6ccaf812639f2
Aaron Meurer <asmeurer@gmail.com>
2014-01-31 11:07:56 -0600

    Cleaup pudb recipe

--- pudb/build.sh
+++ pudb/build.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-touch requirements.txt;
-
-mkdir -vp ${PREFIX}/bin;
-
-${PYTHON} setup.py install || exit 1;
-
-#POST_LINK="${PREFIX}/bin/.pudb-post-link.sh"
-#cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
-#chmod -v 0755 ${POST_LINK};
+python setup.py install
