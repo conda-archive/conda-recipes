a7c551b64f1689d30b78f2faed855bb233caca51
Aaron Meurer <asmeurer@gmail.com>
2014-02-17 14:01:04 -0600

    Add CFLAGS from the makefile

--- minisat+/build.sh
+++ minisat+/build.sh
@@ -1,1 +1,1 @@
-make rx CFLAGS="$CFLAGS -Wall -ffloat-store -I$PREFIX/include"
+make rx CFLAGS= "-Wall -ffloat-store -I$PREFIX/include -IADTs -include Global.h -include Main.h -D_FILE_OFFSET_BITS=64"
