68fbc7a1c8f4f0cb41d1cacfd78e96ed132e0c03
Aaron Meurer <asmeurer@gmail.com>
2014-02-17 14:57:27 -0600

    Also compile the bignum version

--- minisat+/build.sh
+++ minisat+/build.sh
@@ -1,3 +1,4 @@
 make rx CFLAGS="-I$PREFIX/include"
+make rs CFLAGS="-I$PREFIX/include"
 mkdir -p $PREFIX/bin
-cp minisat+_script minisat+_64-bit_static $PREFIX/bin
+cp minisat+_script minisat+_64-bit_static minisat+_bignum_static $PREFIX/bin
