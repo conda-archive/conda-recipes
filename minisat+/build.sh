af0373d7bfb7679b05052aebdd8eafeee696e7c7
Aaron Meurer <asmeurer@gmail.com>
2014-02-18 14:21:29 -0600

    Work on getting minisat+ to use https://github.com/niklasso/minisatp/

--- minisat+/build.sh
+++ minisat+/build.sh
@@ -1,4 +1,2 @@
-make rx CFLAGS="-I$PREFIX/include"
-make rs CFLAGS="-I$PREFIX/include"
-mkdir -p $PREFIX/bin
-cp minisat+_script minisat+_64-bit_static minisat+_bignum_static $PREFIX/bin
+make config prefix=$PREFIX
+make install
