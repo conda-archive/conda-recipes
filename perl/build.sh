b336ece80d6824ad577127c9d74b5f2e729d8323
Dan Blanchard <dblanchard@ets.org>
2014-02-28 14:04:24 -0500

    Remove environment variable stuff from build.sh, because it's unnecessary

--- perl/build.sh
+++ perl/build.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# Save Perl-related environment variables if they were previously set, 
-# but unset them
-echo "Saving and unsetting Perl environment variables"
-OLD_PERL5LIB=$PERL5LIB
-unset PERL5LIB
-OLD_PERL_LOCAL_LIB_ROOT=$PERL_LOCAL_LIB_ROOT
-unset PERL_LOCAL_LIB_ROOT
-OLD_PERL_MB_OPT=$PERL_MB_OPT
-unset PERL_MB_OPT
-OLD_PERL_MM_OPT=$PERL_MM_OPT
-unset PERL_MM_OPT
-
 # Build perl
-echo "Starting configuration"
 sh Configure -de -Dprefix=$PREFIX -Duserelocatableinc
 make
 make test
@@ -22,14 +9,3 @@ make install
 # Install CPAN Minus to make building other packages that rely on this simpler
 echo "Installing CPAN Minus"
 curl -L http://cpanmin.us | perl - App::cpanminus
-
-# Restore environment variables
-echo "Restoring Perl environment variables"
-export PERL5LIB=$OLD_PERL5LIB
-unset OLD_PERL5LIB
-export PERL_LOCAL_LIB_ROOT=$OLD_PERL_LOCAL_LIB_ROOT
-unset OLD_PERL_LOCAL_LIB_ROOT
-export PERL_MB_OPT=$OLD_PERL_MB_OPT
-unset OLD_PERL_MB_OPT
-export PERL_MM_OPT=$OLD_PERL_MM_OPT
-unset OLD_PERL_MM_OPT
