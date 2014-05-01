4da6d6aa031aba0122376c0fd197ff941478420d
Ilan Schnell <ilanschnell@gmail.com>
2014-05-01 14:28:38 -0500

    add tck/tk recipe

---
+++ tcltk/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VER=$PKG_VERSION
+
+cd $SRC_DIR
+tar xzf tcl${VER}-src.tar.gz
+tar xzf tk${VER}-src.tar.gz
+
+cd $SRC_DIR/tcl${VER}/unix
+./configure --prefix=$PREFIX
+make
+make install
+
+cd $SRC_DIR/tk${VER}/unix
+./configure --with-tcl=$PREFIX/lib --prefix=$PREFIX
+make
+make install
+
+cd $PREFIX
+rm -rf man share
