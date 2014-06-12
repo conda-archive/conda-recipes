daa1e4412ca61897c9ff1fc98f2d5c04588b3f56
irritum <kamil.kwiek@continuum.io>
2014-06-12 19:56:48 +0200

    Added recipe for apache-maven-3.2.1 package

---
+++ apache-maven/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Build dependencies:
+# - java-1.7.0-openjdk-devel
+# - appropriately setup JAVA_HOME variable in system
+
+mkdir -vp ${PREFIX}/bin;
+mkdir -vp ${PREFIX}/share;
+
+export JAVA_HOME="/usr/lib/jvm/java"
+export JRE_HOME="/usr/lib/jvm/jre"
+
+cat > ${PREFIX}/bin/mvn <<EOF
+#!/bin/bash
+
+CWD="\$(cd "\$(dirname "\${0}")" && pwd -P)"
+export M2_HOME="\$(cd "\${CWD}/../share/${PKG_NAME}" && pwd -P)"
+export M2="\${M2_HOME}/bin"
+export MAVEN_OPTS="-Xms256m -Xmx512m"
+
+echo -e ""
+echo -e "Setting up M2_HOME for mvn to \${M2_HOME} ..."
+echo -e "Launching mvn ..."
+echo -e ""
+exec \${M2}/mvn "\${@}"
+EOF
+
+chmod 755 ${PREFIX}/bin/mvn || exit 1;
+
+cp -var ${SRC_DIR}/ ${PREFIX}/share || exit 1;
+
+pushd ${PREFIX}/share || exit 1;
+ln -sv ${PKG_NAME}-${PKG_VERSION} ${PKG_NAME} || exit  1;
+popd || exit 1;
+
