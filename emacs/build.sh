03590d8e3edb20935d6042c39b7ce65af08fa9d8
Aaron Meurer <asmeurer@gmail.com>
2014-06-17 16:21:13 -0500

    Clean up emacs recipe

--- emacs/build.sh
+++ emacs/build.sh
@@ -2,9 +2,3 @@
 ./configure  --prefix=$PREFIX --without-x
 
 make -j4  && make -j4  install
-
-# # We need a wrapper script because emacs requires certain environment
-# # variables to be set to find various things.
-# mv $PREFIX/bin/emacs $PREFIX/bin/.emacs
-# cp $RECIPE_DIR/start_emacs.sh $PREFIX/bin/emacs
-# chmod a+x $PREFIX/bin/emacs
