4d2c45b04648a572f0d4058b7d01096fe96e5141
David Li <li.davidm96@gmail.com>
2014-07-02 18:02:16 -0500

    Remove Python dependency from Atom Shell

--- atom-shell/build.sh
+++ atom-shell/build.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 
+BIN=$PREFIX/bin
+EXEC=$BIN/atomshell
+
+mkdir -p $BIN
 mkdir -p $SP_DIR/atomshell
-cp $RECIPE_DIR/*.py $SP_DIR/atomshell/
 
 if [[ (`uname` == Linux) ]]; then
-    wget https://github.com/atom/atom-shell/releases/download/v0.13.3/atom-shell-v0.13.3-linux-x64.zip
-    unzip atom-shell-v0.13.3-linux-x64.zip -d atom-shell
+    mkdir $PREFIX/atom-shell
+    mv ./* $PREFIX/atom-shell
+    chmod +x $PREFIX/atom-shell/atom
 
-    mv atom-shell $SP_DIR/atomshell/atomshell
+    cat <<EOF >$EXEC
+#!/bin/sh
+
+$PREFIX/atom-shell/atom "\$@"
+
+EOF
 fi
 
-
 if [ `uname` == Darwin ]; then
-    curl -OL https://github.com/atom/atom-shell/releases/download/v0.13.3/atom-shell-v0.13.3-darwin-x64.zip
-    unzip atom-shell-v0.13.3-darwin-x64.zip -d atom-shell
-
     # XXX THIS IS A HACK XXX
     rm 'atom-shell/Atom.app/Contents/Frameworks/Atom Framework.framework/Frameworks'
     rm 'atom-shell/Atom.app/Contents/Frameworks/Atom Framework.framework/Libraries/Libraries'
 
-    mv atom-shell/Atom.app $SP_DIR/atomshell/atomshell
+    mv atom-shell/Atom.app $PREFIX/atom-shell
+
+    cat <<EOF >$EXEC
+#!/bin/sh
+
+$PREFIX/atom-shell/Contents/MacOS/Atom "\$@"
+
+EOF
 fi
+
+
+chmod +x $EXEC
