fedc0c2eda4f2a36c2d2c0ad7d3357ff739e00a6
David Li <li.davidm96@gmail.com>
2014-07-03 09:09:40 -0500

    Fix Windows build of atom-shell

--- atom-shell/bld.bat
+++ atom-shell/bld.bat
@@ -1,8 +1,25 @@
-pushd %RECIPE_DIR%
-python download_atom_windows.py
+mkdir %PREFIX%\atom-shell
 if errorlevel 1 exit 1
 
-mkdir %SP_DIR%\atomshell
-move atom-shell %SP_DIR%\atomshell\atomshell
-move %RECIPE_DIR%\*.py %SP_DIR%\atomshell
+xcopy /E .\* %PREFIX%\atom-shell\
 if errorlevel 1 exit 1
+
+mkdir %PREFIX%\Scripts
+if errorlevel 1 exit 1
+
+set bin=%PREFIX%\Scripts\atomshell.bat
+
+echo rem stackoverflow.com/questions/761615 >> %bin%
+echo rem discard the first parameter >> %bin%
+
+echo shift >> %bin%
+echo set params=%%1 >> %bin%
+
+echo :loop >> %bin%
+echo shift >> %bin%
+echo if [%%1]==[] goto endloop >> %bin%
+echo set params=%%params%% %%1 >> %bin%
+echo goto loop >> %bin%
+
+echo :endloop >> %bin%
+echo %PREFIX%\atom-shell\atom.exe %%params%% >> %bin%
