afb979f5a3df931c3cec487b3b06f5bfb76c60c4
David Li <li.davidm96@gmail.com>
2014-07-03 10:53:31 -0500

    Fix Atom Shell handling of cmdline params on Windows

--- atom-shell/bld.bat
+++ atom-shell/bld.bat
@@ -8,18 +8,4 @@ mkdir %SCRIPTS%
 if errorlevel 1 exit 1
 
 set bin=%SCRIPTS%\atomshell.bat
-
-echo rem stackoverflow.com/questions/761615 >> %bin%
-echo rem discard the first parameter >> %bin%
-
-echo shift >> %bin%
-echo set params=%%1 >> %bin%
-
-echo :loop >> %bin%
-echo shift >> %bin%
-echo if [%%1]==[] goto endloop >> %bin%
-echo set params=%%params%% %%1 >> %bin%
-echo goto loop >> %bin%
-
-echo :endloop >> %bin%
-echo /opt/anaconda1anaconda2anaconda3\atom-shell\atom.exe %%params%% >> %bin%
+echo /opt/anaconda1anaconda2anaconda3\atom-shell\atom.exe %%* >> %bin%
