80346f71bdd3775753aa05fab6238e3488a1ae6e
Aaron Meurer <asmeurer@gmail.com>
2014-07-11 11:21:09 -0500

    Fix another flag

--- x264/build.sh
+++ x264/build.sh
@@ -5,7 +5,7 @@ mkdir -vp ${PREFIX}/bin;
 if [[ $ARCH = 64 ]]; then
     export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
 else
-    export CFLAGS="-Wall -g -m64 -pipe -O2 -march=i386 -fPIC"
+    export CFLAGS="-Wall -g -m32 -pipe -O2 -march=i386 -fPIC"
 fi
 export CXXLAGS="${CFLAGS}"
 #export CPPFLAGS="-I${PREFIX}/include"
