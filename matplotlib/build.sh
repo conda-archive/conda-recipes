b019fc61e833e5bebf56f28f1c2de0360a1bfe1e
Aaron Meurer <asmeurer@gmail.com>
2014-07-12 11:20:25 -0500

    Add the matplotlib recipe from Anaconda

---
+++ matplotlib/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ `uname` == Darwin ]; then
+    $REPLACE '#ifdef WITH_NEXT_FRAMEWORK' '#if 1' src/_macosx.m
+fi
+
+cp setup.cfg.template setup.cfg || exit 1
+
+$REPLACE "/usr/local" "$PREFIX" setupext.py
+
+$PYTHON setup.py install
+
+rm -rf $SP_DIR/PySide
