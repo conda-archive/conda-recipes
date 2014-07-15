be53cd8b43e20801dc1ddd8919671d29a699b9de
Ondřej Čertík <ondrej.certik@gmail.com>
2014-07-15 13:00:30 -0600

    gmp: enable C++ bindings
    These are needed e.g. for the csympy package.

--- gmp/build.sh
+++ gmp/build.sh
@@ -3,9 +3,9 @@
 chmod +x configure
 
 if [ `uname` == Darwin ]; then
-    ./configure --prefix=$PREFIX --disable-shared
+    ./configure --prefix=$PREFIX --disable-shared --enable-cxx
 else
-    ./configure --prefix=$PREFIX
+    ./configure --prefix=$PREFIX --enable-cxx
 fi
 
 make
