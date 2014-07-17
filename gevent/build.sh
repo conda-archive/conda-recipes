3b08d556b885e9ac656380a949b5704d746345f0
irritum <kamil.kwiek@continuum.io>
2014-07-17 17:29:24 +0200

    Added recipe for gevent-1.0.1 package

---
+++ gevent/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+${PYTHON} setup.py install || exit 1;
+
