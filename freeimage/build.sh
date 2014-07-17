f4a2e0710960977c4733181cb5a0bc6edee0d30e
irritum <kamil.kwiek@continuum.io>
2014-07-17 14:56:47 +0200

    Adjusted freeimage recipe

--- freeimage/build.sh
+++ freeimage/build.sh
@@ -11,3 +11,11 @@ sed -i 's:-o root -g root::' Makefile* || exit 1;
 
 make || exit 1;
 make install DESTDIR="${PREFIX}" || exit 1;
+
+mkdir -p ${PREFIX}/lib || exit 1;
+mkdir -p ${PREFIX}/include || exit 1;
+
+mv ${PREFIX}/usr/lib/* ${PREFIX}/lib || exit 1;
+mv ${PREFIX}/usr/include/* ${PREFIX}/include || exit 1;
+
+rm -rf ${PREFIX}/usr/ || exit 1;
