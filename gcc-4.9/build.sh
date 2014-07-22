7af3c2d2b38766dd833511fc23365aa2903a4184
Aaron Meurer <asmeurer@gmail.com>
2014-07-22 10:44:36 -0500

    Try disabling multilib for gcc 4.9 on OS X

--- gcc-4.9/build.sh
+++ gcc-4.9/build.sh
@@ -1,10 +1,21 @@
-./configure \
-    --prefix=$PREFIX \
-    --with-gmp=$PREFIX \
-    --with-mpfr=$PREFIX \
-    --with-mpc=$PREFIX \
-    --with-isl=$PREFIX \
-    --with-cloog=$PREFIX
+if [ `uname` == Darwin ]; then
+    ./configure \
+        --prefix=$PREFIX \
+        --with-gmp=$PREFIX \
+        --with-mpfr=$PREFIX \
+        --with-mpc=$PREFIX \
+        --with-isl=$PREFIX \
+        --with-cloog=$PREFIX \
+        --disable-multilib
+else
+    ./configure \
+        --prefix=$PREFIX \
+        --with-gmp=$PREFIX \
+        --with-mpfr=$PREFIX \
+        --with-mpc=$PREFIX \
+        --with-isl=$PREFIX \
+        --with-cloog=$PREFIX
+fi
 
 make
 make install
