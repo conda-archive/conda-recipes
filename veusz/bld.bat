2116ffe86ed076d4e833bfcb22c7e10c7a70f8f2
David Li <li.davidm96@gmail.com>
2014-07-30 10:27:26 -0500

    Fix Veusz build for Windows

--- veusz/bld.bat
+++ veusz/bld.bat
@@ -1,5 +1,5 @@
-%PYTHON% setup.py build
-%PYTHON% setup.py install
+"%PYTHON%" setup.py build
+"%PYTHON%" setup.py install
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
