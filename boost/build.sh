8fcd9f444ea936744ba7f5191b6d96611b5be68c
Patrick Snape <patricksnape@gmail.com>
2014-08-23 16:00:13 +0200

    Move the boost patches into the yaml
    This moves the boost patches into the yaml to keep them
    consistent with other existing packages in the conda
    build ecosystem. No other changes are made.

--- boost/build.sh
+++ boost/build.sh
@@ -11,8 +11,6 @@
 # Build dependencies:
 # - bzip2-devel
 
-PATCH_DIR=${RECIPE_DIR}/1.55.0_patches
-
 mkdir -vp ${PREFIX}/bin;
 
 if [ `uname` == Darwin ]; then
@@ -21,15 +19,6 @@ if [ `uname` == Darwin ]; then
   CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
   LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN} "
   LINKFLAGS="${LINKFLAGS} -stdlib=libc++"
-
-  # Patches boost::atomic for LLVM 3.4 as it is used on OS X 10.9 with Xcode 5.1
-  # https://github.com/Homebrew/homebrew/issues/27396
-  # https://github.com/Homebrew/homebrew/pull/27436
-  patch -Np2 -i ${PATCH_DIR}/6bb71fdd.diff
-  patch -Np2 -i ${PATCH_DIR}/e4bde20f.diff
-  # Patch boost::serialization for Clang
-  # https://svn.boost.org/trac/boost/ticket/8757
-  patch -Np1 -i ${PATCH_DIR}/0005-Boost.S11n-include-missing-algorithm.patch
 
   B2ARGS="toolset=clang"
 
@@ -53,6 +42,3 @@ else
     install | tee b2.log 2>&1
 fi
 
-#POST_LINK="${PREFIX}/bin/.boost-post-link.sh"
-#cp -v ${RECIPE_DIR}/post-link.sh ${POST_LINK};
-#chmod -v 0755 ${POST_LINK};
