e969efcf8a37232044b68689f35936725efb3c74
Bradley Kreider <bkreider@continuum.io>
2014-10-15 23:59:55 -0400

    Fixed postgres for OSX

--- postgresql-9.3/build.sh
+++ postgresql-9.3/build.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
-export CFLAGS="-I$PREFIX/include"
+# The libxml2 include is needed because libxslt includes
+# libxml/*.h  which is a subdirectory
+export CFLAGS="-I$PREFIX/include -I$PREFIX/include/libxml2"
 export LDFLAGS="-L$PREFIX/lib -lxml2 -L$PREFIX/lib -lxslt"
 export LDFLAGS_EX="$LDFLAGS -L$PREFIX/lib -lreadline"
 
 if uname | grep Darwin > /dev/null; then
-    # 10.9 appears to have an issue with libxml2; clang/ld isn't respecting
-    # the flags above, and it attempts to link with /usr/lib/libxml2.2.dynlib,
-    # which results in an error and ./configure time.  @Aaron, have you seen
-    # this before?  Can you reproduce the error?
-    configure --prefix=$PREFIX \
+    # The link step is not the problem.  Testing the a.out
+    # binary fails because it finds the wrong dynlib. 
+    DYLD_LIBRARY_PATH=$PREFIX/lib ./configure --prefix=$PREFIX \
+        --disable-debug        \
         --with-readline        \
         --with-python          \
         --with-openssl         \
-        --with-libxml          \
+        --with-libxml2         \
         --with-libxslt
 else
-    configure --prefix=$PREFIX \
+    ./configure --prefix=$PREFIX \
         --with-readline        \
         --with-python          \
         --with-openssl         \
-        --with-libxml          \
+        --with-libxml2         \
         --with-libxslt
 fi
 
