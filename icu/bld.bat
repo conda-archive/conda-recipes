fc5ce7260b235fb4c11f878a302d4310e0ffb6c0
Darren Dale <dsdale24@gmail.com>
2014-10-26 14:48:20 -0400

    fixes for icu windows recipe: 32/64-bits, paths with spaces

--- icu/bld.bat
+++ icu/bld.bat
@@ -1,3 +1,3 @@
-xcopy lib64\* %PREFIX%\Library\lib\ /E
-xcopy include\* %PREFIX%\Library\include\ /E
-xcopy bin64\*.dll %PREFIX%\Scripts\ /E
+xcopy lib64\* "%PREFIX%\Library\lib\" /E
+xcopy include\* "%PREFIX%\Library\include\" /E
+xcopy bin64\*.dll "%PREFIX%\Scripts\" /E
