bd1d3118917c2ea208b2e0231f42f6d7d82350ce
Aaron Meurer <asmeurer@gmail.com>
2014-10-30 14:12:10 -0500

    Try linking in libgcc and libquadmath

--- pymc/build.sh
+++ pymc/build.sh
@@ -5,6 +5,8 @@ if [ `uname` == Darwin ]; then
     export FFLAGS="-static -ff2c -arch x86_64"
     export LDFLAGS="-Wall -undefined dynamic_lookup -bundle -arch x86_64"
     cp $PREFIX/lib/libgfortran*.*a .
+    cp $PREFIX/lib/libgcc_s* .
+    cp $PREFIX/lib/libquadmath* .
     export LDFLAGS="-Wl,-search_paths_first -L$(pwd) $LDFLAGS"
 fi
 
