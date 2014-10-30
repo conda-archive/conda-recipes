12d4cad9ed6ca1f3be8c47a61792d6d072487f16
Aaron Meurer <asmeurer@gmail.com>
2014-10-30 11:09:38 -0500

    Try statically linking libgfortran into pymc using the same method as scipy

--- pymc/build.sh
+++ pymc/build.sh
@@ -4,6 +4,8 @@ if [ `uname` == Darwin ]; then
     export CFLAGS="-arch x86_64"
     export FFLAGS="-static -ff2c -arch x86_64"
     export LDFLAGS="-Wall -undefined dynamic_lookup -bundle -arch x86_64"
+    cp $PREFIX/lib/libgfortran*.*a .
+    export LDFLAGS="-Wl,-search_paths_first -L$(pwd) $LDFLAGS"
 fi
 
 $PYTHON setup.py build
