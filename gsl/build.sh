6f5f46bd99531b3aff3f4389a53f7eb6256e5c16
Aaron Meurer <asmeurer@gmail.com>
2014-10-31 16:39:26 -0500

    Fix gsl package
    The flags environment variables screw up the build apparently.

--- gsl/build.sh
+++ gsl/build.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-
 ./configure --prefix=$PREFIX
 
 make
