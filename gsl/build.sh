85acef0ab5d2812db3e1d3d1cf111975529218a2
Aaron Meurer <asmeurer@gmail.com>
2014-10-31 16:20:15 -0500

    Add gsl from r-conda-recipes

---
+++ gsl/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export CFLAGS="-I$PREFIX/include"
+export CPPFLAGS="-I$PREFIX/include"
+export CXXFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+
+./configure --prefix=$PREFIX
+
+make
+make check
+make install
