867a9a097c5844bac30546b368682c624a286d8f
Aaron Meurer <asmeurer@gmail.com>
2014-11-07 16:11:58 -0600

    We need a configure for r-tests

--- r-tests/build.sh
+++ r-tests/build.sh
@@ -1,1 +1,14 @@
+
+./configure --with-x                        \
+            --with-pic                      \
+            --prefix=$PREFIX                \
+            --enable-shared                 \
+            --enable-R-shlib                \
+            --enable-BLAS-shlib             \
+            --disable-R-profiling           \
+            --disable-prebuilt-html         \
+            --disable-memory-profiling      \
+            --with-tk-config=$TK_CONFIG     \
+            --with-tcl-config=$TCL_CONFIG
+
 make install-tests
