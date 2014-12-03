f5b79932130e7c69cb3ed213c31137bc479a1529
Aaron Meurer <asmeurer@gmail.com>
2014-12-03 14:54:11 -0600

    Install R on Windows from the official installer

--- r/bld.bat
+++ r/bld.bat
@@ -1,2 +1,8 @@
-cd src\gnuwin32
-make
+@rem cd src\gnuwin32
+@rem make
+
+@rem http://cran.r-project.org/bin/windows/base/rw-FAQ.html#Can-I-customize-the-installation_003f
+@rem The easiest way to configure this is to run the installer with
+@rem /SAVEINF="installer_settings.inf" with the desired settings and cancel just
+@rem before the install begins.
+R-3.1.2-win.exe /SILENT /LOADINF="%RECIPE_DIR%\installer_settings.inf" /DIR="%PREFIX%"
