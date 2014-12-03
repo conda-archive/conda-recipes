575411adbb2ed5b59108c0076aeb1e5c4861296a
Aaron Meurer <asmeurer@gmail.com>
2014-12-03 16:33:47 -0600

    Try building R without the INF file

--- r/bld.bat
+++ r/bld.bat
@@ -5,4 +5,4 @@
 @rem The easiest way to configure this is to run the installer with
 @rem /SAVEINF="installer_settings.inf" with the desired settings and cancel just
 @rem before the install begins.
-R-3.1.2-win.exe /SILENT /LOADINF="%RECIPE_DIR%\installer_settings.inf" /DIR="%PREFIX%"
+R-3.1.2-win.exe /SILENT /DIR="%PREFIX%"
