9aa17f359681524ab0dd0cbd71d5306077f01855
Aaron Meurer <asmeurer@gmail.com>
2014-12-03 17:16:00 -0600

    Revert "Install R on Windows from the official installer"
    This reverts commit f5b79932130e7c69cb3ed213c31137bc479a1529.
    
    Conflicts:
    	r/bld.bat

--- r/bld.bat
+++ r/bld.bat
@@ -1,9 +1,2 @@
-@rem cd src\gnuwin32
-@rem make
-
-@rem http://cran.r-project.org/bin/windows/base/rw-FAQ.html#Can-I-customize-the-installation_003f
-@rem The easiest way to configure this is to run the installer with
-@rem /SAVEINF="installer_settings.inf" with the desired settings and cancel just
-@rem before the install begins.
-R-3.1.2-win.exe /SILENT /DIR="%PREFIX%"
-"%PREFIX%\bin\R.exe"
+cd src\gnuwin32
+make
