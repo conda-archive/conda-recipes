a1a7b12adee9944b7ec44239981a1b3590412c70
Aaron Meurer <asmeurer@gmail.com>
2014-12-04 15:35:31 -0600

    Try 32-bit

--- r/bld.bat
+++ r/bld.bat
@@ -3,10 +3,11 @@ set TMPDIR=.
 
 @rem The path where the Rtools installed the R stuff. Should be a directory
 @rem with a Tcl directory in it.
-set RTOOLS=C:\R64
+set RTOOLS=C:\R
 
 xcopy /S /E "%RTOOLS%\Tcl" "%SRC_DIR%\Tcl\"
 if errorlevel 1 exit 1
+
 
 cd src\gnuwin32
 make
