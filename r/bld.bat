817decfb7812e647062fc29bf5d8dbbb301a34f5
Aaron Meurer <asmeurer@gmail.com>
2014-12-04 13:57:46 -0600

    Set R_HOME in r/bld.bat

--- r/bld.bat
+++ r/bld.bat
@@ -1,4 +1,9 @@
 @rem This is relative apparently
 set TMPDIR=.
+
+@rem The path where the Rtools installed the R stuff. Should be a directory
+@rem with a Tcl directory in it.
+set R_HOME=C:\R64
+
 cd src\gnuwin32
 make
