453a5b50dcb4c077a23f0d32ba7ebbdd2542be10
Aaron Meurer <asmeurer@gmail.com>
2014-12-08 17:59:49 -0600

    Should be gz

--- r/download_win_libs.py
+++ r/download_win_libs.py
@@ -17,7 +17,7 @@ def download_file(url):
     return local_filename
 
 for url in [
-    'http://tcpdiag.dl.sourceforge.net/project/libpng/libpng16/1.6.15/libpng-1.6.15.tar.xz',
+    'http://tcpdiag.dl.sourceforge.net/project/libpng/libpng16/1.6.15/libpng-1.6.15.tar.gz',
     'http://www.ijg.org/files/jpegsrc.v9a.tar.gz',
     'http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz',
     ]:
