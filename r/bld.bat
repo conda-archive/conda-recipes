e5df983e87a2b49a2508cc46482feb97ffe5ad80
Aaron Meurer <asmeurer@gmail.com>
2014-12-08 15:43:01 -0600

    Write up R build for Windows in the bld.bat

--- r/bld.bat
+++ r/bld.bat
@@ -1,3 +1,5 @@
+@rem See notes.md for more information about all of this.
+
 @rem This is relative apparently
 set TMPDIR=.
 
@@ -8,10 +10,53 @@ set RTOOLS=C:\R
 xcopy /S /E "%RTOOLS%\Tcl" "%SRC_DIR%\Tcl\"
 if errorlevel 1 exit 1
 
-
-cd src\gnuwin32
-make
-
+@rem R requires sources for libpng, libjpeg, and libtiff to compile.
+python download_win_libs.py
 if errorlevel 1 exit 1
 
-exit 1
+tar -zxf libpng-1.6.15.tar.gz
+if errorlevel 1 exit 1
+
+mv libpng-1.6.10 libpng
+if errorlevel 1 exit 1
+
+tar -zxf jpegsrc.v9a.tar.gz
+if errorlevel 1 exit 1
+
+mv jpeg-9a jpeg-9
+if errorlevel 1 exit 1
+
+tar -zxf tiff-4.0.3.tar.gz
+if errorlevel 1 exit 1
+
+mv tiff-4.0.3/libtiff .
+if errorlevel 1 exit 1
+
+rm -rf tiff-4.0.3
+if errorlevel 1 exit 1
+
+@rem Various things that are needed to make the docs work.
+cp doc\html\logo.jpg %TMPDIR%
+if errorlevel 1 exit 1
+
+cp library\graphics\help\figures\pch.pdf doc\manual\
+if errorlevel 1 exit 1
+
+cp library\graphics\help\figures\mai.pdf doc\manual\
+if errorlevel 1 exit 1
+
+cp library\graphics\help\figures\oma.pdf doc\manual\
+if errorlevel 1 exit 1
+
+@rem Now actually compile it
+cd src\gnuwin32
+make distribution
+if errorlevel 1 exit 1
+
+@rem And finally collect everything together.
+cd installer
+make imagedir
+if errorlevel 1 exit 1
+
+@rem And install it
+xcopy /S /E R-3.1.2 "%PREFIX%"
---
+++ r/download_win_libs.py
@@ -0,0 +1,18 @@
+import requests
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+    return local_filename
+
+for url in [
+    'http://superb-dca2.dl.sourceforge.net/project/libpng/libpng16/1.6.15/libpng-1.6.15.tar.gz',
+    'http://www.ijg.org/files/jpegsrc.v9a.tar.gz',
+    'http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz',
+    ]:
+    download_file(url)
