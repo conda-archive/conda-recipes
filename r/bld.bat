0066cd837b5e7bfae678a2cbbd40fd8b9e91c77e
Aaron Meurer <asmeurer@gmail.com>
2014-12-11 14:27:46 -0600

    First step for building R in 64-bit Windows

--- r/bld.bat
+++ r/bld.bat
@@ -5,7 +5,11 @@ set TMPDIR=.
 
 @rem The path where the Rtools installed the R stuff. Should be a directory
 @rem with a Tcl directory in it.
-set RTOOLS=C:\R
+if "%ARCH%"=="32" (
+    set RTOOLS=C:\R
+) else (
+    set RTOOLS=C:\R64
+)
 
 xcopy /S /E "%RTOOLS%\Tcl" "%SRC_DIR%\Tcl\"
 if errorlevel 1 exit 1
