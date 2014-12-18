867caecad12f4f801044e070f7c6365088ffb4b2
Aaron Meurer <asmeurer@gmail.com>
2014-12-18 15:35:46 -0600

    Force R to build a recommended package

--- r-packages/mass/build.sh
+++ r-packages/mass/build.sh
@@ -1,1 +1,5 @@
+# R refuses to build packages that mark themselves as Priority: Recommended
+mv DESCRIPTION DESCRIPTION.old
+grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+
 R CMD INSTALL --build .
