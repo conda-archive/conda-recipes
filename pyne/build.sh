b1fca3ef80411ff3d80731d57c530949b250ec5c
Cameron Bates <cameron.r.bates@gmail.com>
2014-12-19 10:39:47 -0800

    update pine recipe

--- pyne/build.sh
+++ pyne/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
+unset LDFLAGS
 ${PYTHON} setup.py install || exit 1;
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-cp build/pyne/lib/*.dylib $PREFIX/lib
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-find . -name '*.so' | xargs chmod +x
-cp build/pyne/lib/*.so $PREFIX/lib
-fi
 
