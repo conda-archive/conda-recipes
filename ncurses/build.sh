9117037a24d77571c971f3fa96878e29ac72af35
immerrr <immerrr@gmail.com>
2014-12-20 11:19:23 +0300

    ncurses: use terminfo databases from host system by default
    Terminal devices (incl. virtual) are generally provided by host system
    and terminfo db should describe their capabilities.  It is more likely
    that host system databases fit the devices than the databases shipped
    with conda.

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 mkdir $PREFIX/lib
+TERMINFO_DIRS=/etc/terminfo:/lib/terminfo:/usr/share/terminfo:$PREFIX/share/terminfo
 
 sh ./configure --prefix=$PREFIX \
-    --without-debug --without-ada --without-manpages \
-    --with-shared --disable-overwrite
-
+    --without-debug \
+    --without-ada \
+    --without-manpages \
+    --with-shared \
+    --disable-overwrite \
+    --disable-termcap \
+    --with-terminfo-dirs=/etc/terminfo:/lib/terminfo:/usr/share/terminfo:$PREFIX/share/terminfo \
+    --with-default-terminfo-dir=/usr/share/terminfo
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
