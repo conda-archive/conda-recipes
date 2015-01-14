427c2e55d06e1892abf10cba7bb7ad9891ab84e9
Julien Schueller <schueller@phimeca.com>
2015-01-14 11:34:33 +0100

    fixed config file handling. added dependency on tbb. disabled hmat.

--- openturns/build.sh
+++ openturns/build.sh
@@ -6,6 +6,10 @@ else
     SO_EXT='so'
 fi
 
+# package must be relocatable
+sed -i "80iimport os" python/src/__init__.py
+sed -i "81ios.environ['OPENTURNS_CONFIG_PATH'] = os.path.realpath(__file__ + '/../../../../../etc/openturns')" python/src/__init__.py
+
 mkdir build
 cd build
 
@@ -16,14 +20,17 @@ cmake \
     -DMUPARSER_LIBRARIES=$PREFIX/lib/libmuparser.${SO_EXT} \
     -DMUPARSER_INCLUDE_DIR=$PREFIX/include \
     -DBOOST_ROOT=$PREFIX \
+    -DTBB_LIBRARY=$PREFIX/lib/libtbb.${SO_EXT} \
+    -DTBB_INCLUDE_DIR=$PREFIX/include/tbb \
     -DSWIG_EXECUTABLE:FILEPATH=$PREFIX/bin/swig \
     -DPYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python${PY_VER} \
     -DPYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER} \
     -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.${SO_EXT} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DUSE_TBB=OFF \
     -DUSE_SPHINX=OFF \
+    -DUSE_HMAT=OFF \
     ..
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
+
