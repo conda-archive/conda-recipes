9f9d91f7ca5f162ebbc2e5820bc9f5b0e250ccb7
Aaron Meurer <asmeurer@gmail.com>
2015-01-20 14:33:41 -0600

    Set BOOST_ROOT

--- rstudio/build.sh
+++ rstudio/build.sh
@@ -2,6 +2,7 @@ mkdir build
 cd build
 
 export LIBR_HOME=$PREFIX/lib/R
+export BOOST_ROOT=$PREFIX
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DRSTUDIO_TARGET=Desktop \
       -DCMAKE_BUILD_TYPE=Release \
