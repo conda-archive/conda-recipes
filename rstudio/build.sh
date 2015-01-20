4c4c989b9927325605a06a30329afd198f7865b4
Aaron Meurer <asmeurer@gmail.com>
2015-01-20 14:30:31 -0600

    Set LIBR_HOME

--- rstudio/build.sh
+++ rstudio/build.sh
@@ -3,6 +3,7 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DRSTUDIO_TARGET=Desktop \
       -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_LIBR_HOME=$PREFIX/lib/R \
       ..
 
 make install
