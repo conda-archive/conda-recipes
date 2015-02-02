90958b66543788e79f66a296a2a14f291a6650a0
José Gómez-Dans <jgomezdans@gmail.com>
2015-02-02 14:16:52 +0000

    Compiles in HDF4, HDF5, SQLITE and NetCDF drivers

--- gdal/build.sh
+++ gdal/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+./configure --with-python --prefix=$PREFIX \
+--with-geos=$PREFIX/bin/geos-config \
+--with-static-proj4=$PREFIX \
+--with-hdf5=$PREFIX \
+--with-hdf4=$PREFIX \
+--with-netcdf=$PREFIX \
+--with-xerces=$PREFIX \
+--with-sqlite=$PREFIX \
+--without-pam \
+--with-python \
+--disable-rpath
 
-bash configure --with-python --prefix=$PREFIX
-make
+make 
 make install
 
-rm -rf $PREFIX/share
+# Copy data files 
+mkdir -p $PREFIX/share/gdal/
+cp data/*csv $PREFIX/share/gdal/
+cp data/*wkt $PREFIX/share/gdal/
+
+
