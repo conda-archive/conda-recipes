d434b8f8227c5c75435faea9cd7d33252c1cb3d5
José Gómez-Dans <jgomezdans@gmail.com>
2015-02-02 14:27:39 +0000

    Back to original...

--- gdal/build.sh
+++ gdal/build.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
-./configure --with-python --prefix=$PREFIX \
---with-geos=$PREFIX/bin/geos-config \
---with-static-proj4=$PREFIX \
---with-hdf5=$PREFIX \
---with-hdf4=$PREFIX \
---with-netcdf=$PREFIX \
---with-xerces=$PREFIX \
---with-sqlite=$PREFIX \
---without-pam \
---with-python \
---disable-rpath
 
-make 
+bash configure --with-python --prefix=$PREFIX
+make
 make install
 
-# Copy data files 
-mkdir -p $PREFIX/share/gdal/
-cp data/*csv $PREFIX/share/gdal/
-cp data/*wkt $PREFIX/share/gdal/
-
-
+rm -rf $PREFIX/share
