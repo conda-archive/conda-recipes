2dbb30ebf459ae7bc1449e4584a75612e4cd7585
José Gómez-Dans <jgomezdans@gmail.com>
2015-02-02 14:42:53 +0000

    Updated GDAL to 1.11.1, but also added HDF4, HDF5, LibnetCDF formats, and dependencies. Added a short script to test opening MODIS HDF files (often requested by users), as well as listing the supported formats, both for vetor and raster. Only tested on Linux X86, should be easy to extend to MacOSX

--- gdal/run_test.py
+++ gdal/run_test.py
@@ -7,9 +7,28 @@ import gdal
 import gdalconst
 import ogr
 import osr
+import urllib2
 
+print( "OGR Vector drivers")
+print("==================")
 cnt = ogr.GetDriverCount()
+for i in range(cnt):
+    print(ogr.GetDriver(i).GetName())
+
+print ("GDAL Raster drivers")
+print ("===================")
+cnt = gdal.GetDriverCount()
 for i in xrange(cnt):
-    print ogr.GetDriver(i).GetName()
+    print (gdal.GetDriver(i).LongName)
+
+print ("Total number of vector drivers: %d" % ogr.GetDriverCount())
+print ("Total number of vector drivers: %d" % gdal.GetDriverCount())
+
+print ("Testing opening a MODIS file...")
+fp = open ( "MCD45A1.A2006213.h17v04.051.2013060204001.hdf", 'w')
+fp.write ( urllib2.urlopen("http://e4ftl01.cr.usgs.gov/MOTA/MCD45A1.051/2006.08.01/MCD45A1.A2006213.h17v04.051.2013060204001.hdf").read() )
+fp.close()
+g = gdal.Open ( "MCD45A1.A2006213.h17v04.051.2013060204001.hdf" )
+print ( g.GetSubDatasets() )
 
 import os1_hw
