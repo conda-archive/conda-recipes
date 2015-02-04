12d4c53366ff5a3e5e6f235b504f951eb1ca7c9c
Aaron Meurer <asmeurer@gmail.com>
2015-02-04 15:07:36 -0600

    Fix gcc 4.8 build.sh to not be OS X specific

--- gcc-4.8/build.sh
+++ gcc-4.8/build.sh
@@ -1,19 +1,28 @@
 if [ "$(uname)" == "Darwin" ]; then
     export LDFLAGS="-Wl,-headerpad_max_install_names"
     export BOOT_LDFLAGS="-Wl,-headerpad_max_install_names"
+
+    ./configure \
+        --prefix=$PREFIX \
+        --libdir=$PREFIX/lib \
+        --with-gmp=$PREFIX \
+        --with-mpfr=$PREFIX \
+        --with-mpc=$PREFIX \
+        --with-isl=$PREFIX \
+        --with-cloog=$PREFIX \
+        --with-boot-ldflags=$LDFLAGS \
+        --with-stage1-ldflags=$LDFLAGS \
+        --disable-multilib
+else
+    ./configure \
+        --prefix=$PREFIX \
+        --libdir=$PREFIX/lib \
+        --with-gmp=$PREFIX \
+        --with-mpfr=$PREFIX \
+        --with-mpc=$PREFIX \
+        --with-isl=$PREFIX \
+        --with-cloog=$PREFIX \
+        --disable-multilib
 fi
-
-./configure \
-    --prefix=$PREFIX \
-    --libdir=$PREFIX/lib \
-    --with-gmp=$PREFIX \
-    --with-mpfr=$PREFIX \
-    --with-mpc=$PREFIX \
-    --with-isl=$PREFIX \
-    --with-cloog=$PREFIX \
-    --with-boot-ldflags=$LDFLAGS \
-    --with-stage1-ldflags=$LDFLAGS \
-    --disable-multilib
-
 make
 make install
