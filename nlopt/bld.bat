a0568549a40482b99acaa851329d320b94917741
Aaron Meurer <asmeurer@gmail.com>
2015-02-13 16:47:03 -0600

    Revert "Try adding -DCMAKE_BUILD_TYPE=Release"
    This reverts commit 9fbcc01a402f00a63c6031187524a7e99ca60d2b.

--- nlopt/bld.bat
+++ nlopt/bld.bat
@@ -3,5 +3,5 @@ cp "%RECIPE_DIR%\CMakeLists.txt" .
 if errorlevel 1 exit 1
 cp "%RECIPE_DIR%\config.cmake.h.in" .
 if errorlevel 1 exit 1
-cmake -DCMAKE_INSTALL_PREFIX="%PREFIX%" -DCMAKE_BUILD_TYPE=Release .
+cmake -DCMAKE_INSTALL_PREFIX="%PREFIX%" .
 if errorlevel 1 exit 1
