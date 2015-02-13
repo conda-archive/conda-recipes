9fbcc01a402f00a63c6031187524a7e99ca60d2b
Aaron Meurer <asmeurer@gmail.com>
2015-02-13 16:22:43 -0600

    Try adding -DCMAKE_BUILD_TYPE=Release

--- nlopt/bld.bat
+++ nlopt/bld.bat
@@ -3,5 +3,5 @@ cp "%RECIPE_DIR%\CMakeLists.txt" .
 if errorlevel 1 exit 1
 cp "%RECIPE_DIR%\config.cmake.h.in" .
 if errorlevel 1 exit 1
-cmake -DCMAKE_INSTALL_PREFIX="%PREFIX%" .
+cmake -DCMAKE_INSTALL_PREFIX="%PREFIX%" -DCMAKE_BUILD_TYPE=Release .
 if errorlevel 1 exit 1
