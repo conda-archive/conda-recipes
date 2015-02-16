6b63898b1ecc01a5d32332df9062a75eb8b08396
Cameron Bates <cameron.r.bates@gmail.com>
2015-02-16 11:46:08 -0800

    use latest stable release

--- pyne/build.sh
+++ pyne/build.sh
@@ -2,3 +2,10 @@
 unset LDFLAGS
 ${PYTHON} setup.py install || exit 1;
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+cp build/pyne/lib/*.dylib $PREFIX/lib
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+find . -name '*.so' | xargs chmod +x
+cp build/pyne/lib/*.so $PREFIX/lib
+fi
+
