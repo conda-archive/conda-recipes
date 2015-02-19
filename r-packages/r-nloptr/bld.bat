44b50966d58152539c5460e4634c824eeed24386
Aaron Meurer <asmeurer@gmail.com>
2015-02-19 14:04:12 -0600

    Try building nlopt inside of nloptr, as suggested in the nloptr docs

--- r-packages/r-nloptr/bld.bat
+++ r-packages/r-nloptr/bld.bat
@@ -1,5 +1,12 @@
-@rem cygwin wants a path with forward slashes
-set NLOPT_HOME=%PREFIX:\=/%
+@rem Download the nlopt source
+python "%RECIPE_DIR%\download_nlopt.py"
+if errorlevel 1 exit 1
+
+tar -zxf nlopt-2.4.2.tar.gz
+if errorlevel 1 exit 1
+
+bash "%RECIPE_DIR%\install_windows.sh"
+if errorlevel 1 exit 1
 
 "%R%" CMD INSTALL --build .
 if errorlevel 1 exit 1
