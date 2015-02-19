2e32f0602728a0a5e2eb8d27dba2609c88d73fda
Aaron Meurer <asmeurer@gmail.com>
2015-02-19 14:41:56 -0600

    Set the filename for the config.guess and config.sub files correctly

--- r-packages/r-nloptr/download_nlopt.py
+++ r-packages/r-nloptr/download_nlopt.py
@@ -1,8 +1,8 @@
 import requests
 
-def download_file(url):
+def download_file(url, fn=None):
     urlparts = requests.packages.urllib3.util.url.parse_url(url)
-    local_filename = urlparts.path.split('/')[-1]
+    local_filename = fn or urlparts.path.split('/')[-1]
 
     print("Downloading %s" % local_filename)
     r = requests.get(url, stream=True, allow_redirects=True)
@@ -16,5 +16,7 @@ def download_file(url):
 
 if __name__ == '__main__':
     download_file('http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz')
-    download_file('http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD')
-    download_file('http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD')
+    download_file('http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD',
+        fn='config.guess')
+    download_file('http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD',
+        fn=config.sub')
