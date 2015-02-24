2cf91674996762c290526305e5b8dec3d93cb506
Carlos Cordoba <ccordoba12@gmail.com>
2015-02-24 16:39:35 -0500

    PyQt4: Update to latest version and make recipe to build on Linux

--- pyqt/build.sh
+++ pyqt/build.sh
@@ -1,29 +1,16 @@
 if [ `uname` == Linux ]; then
     export QMAKESPEC="linux-g++"
-
-
-    # Work around qt's hard-coded paths
-    sudo mkdir -p /opt/anaconda1anaconda2anaconda3/
-    sudo chmod 777 /opt/anaconda1anaconda2anaconda3
-    ln -s $PREFIX/mkspecs /opt/anaconda1anaconda2anaconda3/mkspecs
-    ln -s $PREFIX/include /opt/anaconda1anaconda2anaconda3/include
-    ln -s $PREFIX/lib /opt/anaconda1anaconda2anaconda3/lib
-    ln -s $PREFIX/bin /opt/anaconda1anaconda2anaconda3/bin
-
 fi
 
 if [ `uname` == Darwin ]; then
-    export DYLD_LIBRARY_PATH=$PREFIX/lib/
+    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/
 fi
 
-$PYTHON configure-ng.py --verbose \
-        --confirm-license \
-        --bindir=$PREFIX/bin \
-        --destdir=$SP_DIR \
+$PYTHON configure.py \
+          --verbose \
+          --confirm-license \
+          --bindir=$PREFIX/bin \
+          --destdir=$SP_DIR
 
 make
 make install
-
-if [ `uname` == Linux ]; then
-    sudo rm -rf /opt/anaconda1anaconda2anaconda3
-fi
