1bb126dae3217af7bcbc6e9e54f614de8d8f4ac7
David Froger <david.froger@inria.fr>
2015-03-01 18:01:11 +0100

    Build HDF5 C++ library

--- hdf5/build.sh
+++ hdf5/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --enable-linux-lfs --with-zlib=$PREFIX --with-ssl
+./configure --prefix=$PREFIX --enable-linux-lfs --enable-cxx --with-zlib=$PREFIX --with-ssl
 make
 make install
 
