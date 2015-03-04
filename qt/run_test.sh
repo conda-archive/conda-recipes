3c21e979bbd5926dc9bd9b39aafbd6fcb7a252c9
Carlos Cordoba <ccordoba12@gmail.com>
2015-03-04 13:39:55 -0500

    Qt: Run tests only if they were built
    - I don't know why but they are not built on CentOS

--- qt/run_test.sh
+++ qt/run_test.sh
@@ -3,18 +3,20 @@
 export QML_IMPORT_PATH=$PREFIX/imports
 export FONTCONFIG_PATH=$PREFIX/etc/fonts
 
-$PREFIX/tests/qt4/tst_hybridPixmap/hybridPixmap
-$PREFIX/tests/qt4/tst_qdeclarativewebview/tst_qdeclarativewebview
-$PREFIX/tests/qt4/tst_qgraphicswebview/tst_qgraphicswebview
-$PREFIX/tests/qt4/tst_qwebelement/tst_qwebelement
-$PREFIX/tests/qt4/tst_qwebframe/tst_qwebframe
-$PREFIX/tests/qt4/tst_qwebhistory/tst_qwebhistory
-$PREFIX/tests/qt4/tst_qwebhistoryinterface/tst_qwebhistoryinterface
-$PREFIX/tests/qt4/tst_qwebinspector/tst_qwebinspector
-$PREFIX/tests/qt4/tst_qwebview/tst_qwebview
+if [ -d $PREFIX/tests/qt4 ]; then
+  $PREFIX/tests/qt4/tst_hybridPixmap/hybridPixmap
+  $PREFIX/tests/qt4/tst_qdeclarativewebview/tst_qdeclarativewebview
+  $PREFIX/tests/qt4/tst_qgraphicswebview/tst_qgraphicswebview
+  $PREFIX/tests/qt4/tst_qwebelement/tst_qwebelement
+  $PREFIX/tests/qt4/tst_qwebframe/tst_qwebframe
+  $PREFIX/tests/qt4/tst_qwebhistory/tst_qwebhistory
+  $PREFIX/tests/qt4/tst_qwebhistoryinterface/tst_qwebhistoryinterface
+  $PREFIX/tests/qt4/tst_qwebinspector/tst_qwebinspector
+  $PREFIX/tests/qt4/tst_qwebview/tst_qwebview
+fi
 
 # Skip these two tests because they don't finish
-# on Mac, although they pass
+# on Mac, although they seem to pass
 # $PREFIX/tests/qt4/tst_loading/tst_loading
 # $PREFIX/tests/qt4/tst_painting/tst_painting
 
