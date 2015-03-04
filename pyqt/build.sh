400eede8c7222a09f67595f66f585fcdb0e3180e
Carlos Cordoba <ccordoba12@gmail.com>
2015-03-04 14:36:53 -0500

    PyQt4: Fix build on Linux

--- pyqt/build.sh
+++ pyqt/build.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+
 if [ `uname` == Linux ]; then
     export QMAKESPEC="linux-g++"
+
+    # Add qt.conf to the right place in $SRC_DIR so that
+    # configure.py can run correctly
+    cp $PREFIX/bin/qt.conf $SRC_DIR
 fi
 
 if [ `uname` == Darwin ]; then
     export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/
 
-    # Add qt.conf to the right place in $SRC_DIR so that configure
-    # can run correctly
+    # Add qt.conf to the right place in $SRC_DIR so that
+    # configure.py can run correctly
     QTCONF_PLACE=$SRC_DIR/qtdirs.app/Contents/Resources
     mkdir -p $QTCONF_PLACE
     cp $PREFIX/bin/qt.conf $QTCONF_PLACE
