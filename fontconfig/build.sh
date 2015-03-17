76a884664fc2c3faf3470e0e1da32137b6b45113
Carlos Cordoba <ccordoba12@gmail.com>
2015-03-17 08:56:53 -0500

    Fontconfig: Don't add docs to the package

--- fontconfig/build.sh
+++ fontconfig/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 chmod +x configure
+
 # export FREETYPE_LIBS=-L$PREFIX/lib/
 # export FREETYPE_CFLAGS=-I$PREFIX/include/freetype2
 # export LIBS=-L$PREFIX/lib/
 # export CFLAGS=-I$PREFIX/include
 # export LIBXML2_CFLAGS=-I$PREFIX/include/libxml2
 # export LIBXML2_LIBS=-L$PREFIX/lib/
-./configure --prefix $PREFIX --enable-libxml2
+
+./configure --prefix $PREFIX --enable-libxml2 --disable-docs
 make
 make install
