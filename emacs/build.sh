7cdf5cdf42a9add470d08d2f65e36adabed214f4
Aaron Meurer <asmeurer@gmail.com>
2015-03-25 15:23:37 -0500

    Add --x-includes and --x-libraries to emacs build script on Linux

--- emacs/build.sh
+++ emacs/build.sh
@@ -1,7 +1,7 @@
 if [ "$(uname)" == "Darwin" ]; then
     ./configure  --prefix=$PREFIX --without-x
 else
-    ./configure  --prefix=$PREFIX
+    ./configure  --prefix=$PREFIX --x-includes=$PREFIX/include --x-liibraries=$PREFIX/lib
 fi
 
 make -j4  && make -j4  install
