7bbdce347e8b87967ab698a3e0efd25aa568dbc2
Aaron Meurer <asmeurer@gmail.com>
2015-03-25 15:07:42 -0500

    Build emacs with X on Linux

--- emacs/build.sh
+++ emacs/build.sh
@@ -1,3 +1,7 @@
-./configure  --prefix=$PREFIX --without-x
+if [ "$(uname)" == "Darwin" ]; then
+    ./configure  --prefix=$PREFIX --without-x
+else
+    ./configure  --prefix=$PREFIX
+fi
 
 make -j4  && make -j4  install
