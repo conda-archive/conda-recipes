c408284e136c13ff603ac23c17ff880809e80ab6
Travis Swicegood <development@domain51.com>
2015-04-13 10:30:45 -0700

    Add activate/deactivate to handle setting GDAL_DATA

--- gdal/build.sh
+++ gdal/build.sh
@@ -10,3 +10,11 @@ bash configure \
     --prefix=$PREFIX
 make
 make install
+
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/posix/activate.sh $ACTIVATE_DIR/gdal-activate.sh
+cp $RECIPE_DIR/posix/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
