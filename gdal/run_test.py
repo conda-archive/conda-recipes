fa33f690f55da22c145d09c79d34881addb8051b
Travis Swicegood <development@domain51.com>
2015-04-13 09:54:41 -0700

    Bring in test that @kpurdon and I talked about at pycon

--- gdal/run_test.py
+++ gdal/run_test.py
@@ -16,3 +16,7 @@ driver = gdal.GetDriverByName("netCDF")
 assert driver is not None
 
 import os1_hw
+
+ref = osr.SpatialReference()
+# Raises error if data is not available
+ref.ImportFromEPSG(4326)
