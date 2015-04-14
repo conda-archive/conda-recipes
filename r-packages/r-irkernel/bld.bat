aa6dcea5e30638869a37d1a64ba694afe320f346
Aaron Meurer <asmeurer@gmail.com>
2015-04-14 17:13:23 -0500

    Install the r-irkernel kernel in Windows

--- r-packages/r-irkernel/bld.bat
+++ r-packages/r-irkernel/bld.bat
@@ -1,8 +1,8 @@
 "%R%" CMD INSTALL --build .
 if errorlevel 1 exit 1
 
-@rem Add more build steps here, if they are necessary.
+mkdir "%PREFIX%\share\jupyter\kernels\ir"
+if errorlevel 1 exit 1
 
-@rem See
-@rem http://docs.continuum.io/conda/build.html
-@rem for a list of environment variables that are set during the build process.
+xcopy /s inst\kernelspec\* "%PREFIX%\share\jupyter\kernels\ir\"
+if errorlevel 1 exit 1
