41e773571706e91a54e51e34ad719b7396b07a59
Aaron Meurer <asmeurer@gmail.com>
2015-04-17 16:28:54 -0500

    Fix installation in the netpbm recipe

--- netpbm/build.sh
+++ netpbm/build.sh
@@ -5,4 +5,15 @@ cp $RECIPE_DIR/config.mk.linux config.mk
 sed -i -e "s:/usr/local:$PREFIX:g" config.mk
 
 make
-make install
+make package pkgdir=pkg
+make check
+
+# The netpbm install script is interactive, so just install it manually
+cp -R pkg/bin/* $PREFIX/bin/
+cp -R pkg/lib/* $PREFIX/lib/
+cp -R pkg/link/* $PREFIX/lib/
+cp -R pkg/include/* $PREFIX/include/
+cp -R pkg/man/man1/* $PREFIX/share/man1/
+cp -R pkg/man/man3/* $PREFIX/share/man3/
+cp -R pkg/man/man5/* $PREFIX/share/man5/
+cp -R pkg/man/web/* $PREFIX/share/web/
