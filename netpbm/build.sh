ba92223c4b76a858707da2c101225641dcdef31d
Aaron Meurer <asmeurer@gmail.com>
2015-04-17 15:36:58 -0500

    Make some fixes to the netpbm Makefile

--- netpbm/build.sh
+++ netpbm/build.sh
@@ -1,5 +1,7 @@
 cp $RECIPE_DIR/Makefile.config.linux Makefile.config
 sed -i -e "s:/usr/local:$PREFIX:g" Makefile.config
+sed -i -e "s:JPEGLIB = NONE:#JPEGLIB = $PREFIX/lib/libjpeg.so:g" Makefile.config
+sed -i -e "s:#JPEGHDR_DIR = $PREFIX/include:JPEGHDR_DIR = $PREFIX/include:g" Makefile.config
 
 make
 make install
