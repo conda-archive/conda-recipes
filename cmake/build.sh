80038b5caa2298f12ae07d1ed996901d8c79eaef
Julien Schueller <schueller@phimeca.com>
2015-04-24 08:47:55 +0200

    update to 3.2.2 & parallel build

--- cmake/build.sh
+++ cmake/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CC=cc CXX=c++ ./configure --prefix=$PREFIX
+CC=cc CXX=c++ ./configure --prefix=$PREFIX --parallel=${CPU_COUNT}
 make -j${CPU_COUNT}
 rm $SRC_DIR/Modules/CPack.OSXScriptLauncher.in
 make install
