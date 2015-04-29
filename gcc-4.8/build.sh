b62391f8e319d3bd67d70067d6ae5e4a67cc7180
Stuart Berg <bergs@janelia.hhmi.org>
2015-04-29 17:18:07 -0400

    gcc-4.8: After build, remove "fixed" headers.

--- gcc-4.8/build.sh
+++ gcc-4.8/build.sh
@@ -31,3 +31,18 @@ fi
 make
 make install
 rm $PREFIX/lib64
+
+
+# Lastly, remove the headers that gcc "fixed".
+# They kill the gcc binary's portability to other systems,
+#   and shouldn't be necessary on ANSI-compliant systems anyway.
+# More discussion can be found here:
+# https://groups.google.com/a/continuum.io/d/msg/conda/HwUazgD-hJ0/aofO0vD-MhcJ
+# (We can skip this step on Mac, because the "fixed" 
+#  headers on Mac seem to be forward compatible.)
+if [ "$(uname)" != "Darwin" ]; then
+    while read x ; do
+      grep -q 'It has been auto-edited by fixincludes from' "${x}" \
+               && rm -f "${x}"
+    done < <(find ${PREFIX}/lib/gcc/*/*/include*/ -name '*.h')
+fi
