7ef94734b90abad6f79cb2b9c2e44f8cb7a91bcd
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-03 15:01:47 +0000

    gcc-4.8: post-link: Simplified method of locating crt1.o, etc.

--- gcc-4.8/post-link.sh
+++ gcc-4.8/post-link.sh
@@ -5,22 +5,16 @@ if [ "$(uname)" != "Darwin" ]; then
     # Locate the system's C-runtime object files and link them into the gcc 
     #  build so they are automatically on the gcc search path.
     # (The location of these files varies from one system to the next.)
-    C_RUNTIME_OBJ_FILES="crt1.o crti.o crtn.o"
+    C_RUNTIME_OBJ_FILES="crt0.o crt1.o crt2.o crt3.o crti.o crtn.o"
     SYSTEM_GCC=/usr/bin/gcc
-    
-    DUMMY_PROGRAM="int main(){return 0;}"
-    GCC_CMDS=`echo "$DUMMY_PROGRAM" | $SYSTEM_GCC -v -xc - 2>&1`
-    COLLECT2_FILES=`echo "$GCC_CMDS" | grep collect2`
-    
-    for path in $COLLECT2_FILES; do
-        for obj_file in $C_RUNTIME_OBJ_FILES; do
-            if echo $path | grep -qs $obj_file; then
-                echo "Linking $path into gcc build"
-                ln -s $path ${PREFIX}/lib/gcc/*/*/
-            fi
-        done
-    done
 
+    for obj_file in $C_RUNTIME_OBJ_FILES; do
+	obj_file_full_path=`gcc -print-file-name=$obj_file`
+	if [[ $obj_file_full_path != $obj_file ]]; then
+	    ln -s $obj_file_full_path ${PREFIX}/lib/gcc/*/*/
+	fi
+    done
+    
     # Some distros use different system include paths than the ones this gcc binary was built for.
     # We'll add these to the standard include path by providing a custom "specs file"
 
@@ -30,9 +24,7 @@ if [ "$(uname)" != "Darwin" ]; then
     gcc -dumpspecs > ${SPECS_FILE}
     
     # Now add extra include paths to the specs file, one at a time.
-    # (So far we only know of one (from Ubuntu).)
-    # TODO: Conceivably, we could extract these paths from the $GCC_CMDS output we obtained above,
-    #       but hard-coding these paths is easier for now.
+    # (So far we only know of one: from Ubuntu.)
     EXTRA_SYSTEM_INCLUDE_DIRS="/usr/include/x86_64-linux-gnu"
     
     for INCDIR in ${EXTRA_SYSTEM_INCLUDE_DIRS}; do
