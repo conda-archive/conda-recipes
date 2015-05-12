36486f5b6eb3accb91dcd0f7812b9ed567b8ba12
Ryan Grout <rgrout@continuum.io>
2015-05-11 20:44:51 -0500

    update gdal for python bindings-only package.

--- gdal/bld.bat
+++ gdal/bld.bat
@@ -1,7 +1,19 @@
-for %%x in (gdal.py gdalconst.py gdalnumeric.py ogr.py osr.py osgeo) do (
-    move %SRC_DIR%\%%x %SP_DIR%
-    if errorlevel 1 exit 1
-)
-
-move %SRC_DIR%\GDAL-%PKG_VERSION%.dist-info %SP_DIR%
+%PYTHON% setup.py build_ext --include-dirs %LIBRARY_INC% --library-dirs %LIBRARY_LIB% --gdal-config %LIBRARY_BIN%\gdal-config
 if errorlevel 1 exit 1
+
+%PYTHON% setup.py build_py
+if errorlevel 1 exit 1
+
+%PYTHON% setup.py build_scripts
+if errorlevel 1 exit 1
+
+%PYTHON% setup.py install
+if errorlevel 1 exit 1
+
+REM copy gdal111.dll to python directory
+copy %LIBRARY_BIN%\gdal111.dll %SP_DIR%\osgeo\
+if errorlevel 1 exit 1
+
+if %PY3K%==1 (
+    del %PREFIX%\Lib\lib2to3\*.pickle
+)
--- gdal/build.sh
+++ gdal/build.sh
@@ -1,26 +1,4 @@
-#!/bin/bash
-
-# http://www.michael-joost.de/gdal_install.html
-unset CC CPP CXX
-export MAKEFLAGS=' -j8'
-
-if [ `uname` == Darwin ]; then
-    export LDFLAGS="$LDFLAGS -headerpad_max_install_names -liconv"
-fi
-
-export PYTHON=
-bash configure \
-    --without-python \
-    --with-hdf5=$PREFIX \
-    --with-netcdf=$PREFIX \
-    --prefix=$PREFIX
-make
-make install
-
-#ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-#DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-#mkdir -p $ACTIVATE_DIR
-#mkdir -p $DEACTIVATE_DIR
-
-#cp $RECIPE_DIR/posix/activate.sh $ACTIVATE_DIR/gdal-activate.sh
-#cp $RECIPE_DIR/posix/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
+$PYTHON setup.py build_ext --include-dirs $INCLUDE_PATH --library-dirs $LIBRARY_PATH --gdal-config gdal-config
+$PYTHON setup.py build_py
+$PYTHON setup.py build_scripts
+$PYTHON setup.py install
