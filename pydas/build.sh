722683a4d8674a04598920e7ec6df1f91f1dfa89
Richard West <r.west@neu.edu>
2015-05-19 10:51:07 -0400

    PyDAS: Export the compiler settings so they may do something.

--- pydas/build.sh
+++ pydas/build.sh
@@ -1,6 +1,6 @@
-CC=${PREFIX}/bin/gcc
-CXX=${PREFIX}/bin/g++
-F77=${PREFIX}/bin/gfortran
+export CC=${PREFIX}/bin/gcc
+export CXX=${PREFIX}/bin/g++
+export F77=${PREFIX}/bin/gfortran
 wget -P daspk31 http://www.cs.ucsb.edu/~cse/Software/daspk31.tgz
 make
 $PYTHON setup.py daspk install
