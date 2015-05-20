fe6bee62418f390be5d309942980743470e1527e
Aaron Meurer <asmeurer@gmail.com>
2015-05-20 17:04:19 -0500

    Use clang to compile z3 on OS X

--- z3/build.sh
+++ z3/build.sh
@@ -1,4 +1,8 @@
-python scripts/mk_make.py --prefix=$PREFIX
+if [ `uname` == Darwin ]; then
+    CXX=clang++ CC=clang python scripts/mk_make.py --prefix=$PREFIX
+else
+    python scripts/mk_make.py --prefix=$PREFIX
+fi
 cd build
 make
 make install
