c3cb5b80ba569cca02306f3f2e845e5e233b6114
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-06-16 00:27:21 -0400

    go: Fix so that it simply moves the directory to $PREFIX and installs. Also, use clang on Mac otherwise the build will break when it is relocated.

--- go/build.sh
+++ go/build.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Tell go where it will be installed is not the same as where it is being built.
-export GOROOT_FINAL=$PREFIX
+# Move the go directory into $PREFIX/ so it can be built in its install location.
+cd .. && mv go $PREFIX/ && cd $PREFIX/go
 
 # Change to `src` build and then move out.
 cd src
 ./all.bash
 cd ..
 
-# Install everything manually.
-# Note go expects to find `src` for some operations so it **must** be installed.
-test -d bin && mkdir -p $PREFIX/bin && mv bin/* $PREFIX/bin
-test -d include && mkdir -p $PREFIX/include && mv include/* $PREFIX/include
-test -d lib && mkdir -p $PREFIX/lib && mv lib/* $PREFIX/lib
-test -d pkg && mkdir -p $PREFIX/pkg && mv pkg/* $PREFIX/pkg
-test -d sbin && mkdir -p $PREFIX/sbin && mv sbin/* $PREFIX/sbin
-test -d slib && mkdir -p $PREFIX/slib && mv slib/* $PREFIX/slib
-test -d src && mkdir -p $PREFIX/src && mv src/* $PREFIX/src
-rm -rf $PREFIX/doc/go && mkdir -p $PREFIX/doc/go && mv doc/* $PREFIX/doc/go
+# Link binaries.
+mkdir -p $PREFIX/bin
+ln -s $PREFIX/go/bin/go $PREFIX/bin/go
+ln -s $PREFIX/go/bin/gofmt $PREFIX/bin/gofmt
---
+++ go/post-link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Rerun the script to update the paths.
+cd $PREFIX/go/src
+./all.bash
