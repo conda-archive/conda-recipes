77c076c5e90a6688957fdd31562a1f8e25c16916
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-07-01 11:55:38 -0400

    freeimage/build.sh: Some truly ugly hacks to get it to build.

--- freeimage/build.sh
+++ freeimage/build.sh
@@ -5,7 +5,13 @@ export LDFLAGS="-L${PREFIX}/lib"
 
 cd ${SRC_DIR} || exit 1;
 
-sed -i 's:-o root -g root::' Makefile* || exit 1;
+LANG="C" sed -i.bak 's:-o root -g root::' Makefile* || exit 1;
+LANG="C" sed -i.bak 's/-arch ppc//g' Makefile.osx || exit 1;
+LANG="C" sed -i.bak 's/MacOSX10.5.sdk/MacOSX10.9.sdk/g' Makefile.osx || exit 1;
+LANG="C" sed -i.bak 's/\/Developer\/SDKs/\/Applications\/Xcode.app\/Contents\/Developer\/Platforms\/MacOSX.platform\/Developer\/SDKs/g' Makefile.osx || exit 1;
+LANG="C" sed -i.bak 's/gcc-4.0/gcc -I\/usr\/include -L\/usr\/lib/g' Makefile.osx || exit 1;
+LANG="C" sed -i.bak 's/g++-4.0/g++ -I\/usr\/include -L\/usr\/lib/g' Makefile.osx || exit 1;
+LANG="C" sed -i.bak 's/COMPILERFLAGS = /COMPILERFLAGS = -D__ANSI__ /g' Makefile.osx || exit 1;
 
 make || exit 1;
 make install DESTDIR="${PREFIX}" || exit 1;
