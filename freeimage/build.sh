a650ca2e14f0f03b137b443b7a075ff7a138f323
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-07-01 13:25:36 -0400

    freeimage/build.sh: Restructure directory creation and the like.

--- freeimage/build.sh
+++ freeimage/build.sh
@@ -9,12 +9,8 @@ LANG="C" sed -i.bak 's:-o root -g root::' Makefile* || exit 1;
 cp ${RECIPE_DIR}/Makefile.osx Makefile.osx
 
 make || exit 1;
-make install PREFIX="${PREFIX}" DESTDIR="${PREFIX}" || exit 1;
 
 mkdir -p ${PREFIX}/lib || exit 1;
 mkdir -p ${PREFIX}/include || exit 1;
 
-mv ${PREFIX}/usr/lib/* ${PREFIX}/lib || exit 1;
-mv ${PREFIX}/usr/include/* ${PREFIX}/include || exit 1;
-
-rm -rf ${PREFIX}/usr/ || exit 1;
+make install PREFIX="${PREFIX}" DESTDIR="${PREFIX}" || exit 1;
