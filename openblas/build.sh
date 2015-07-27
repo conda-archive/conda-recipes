687e1c6c74da8b380ca64f0ffa967c79cb69a62e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-07-27 01:21:25 -0400

    openblas/build.sh: Make sure that OpenBLAS appears as the normal BLAS.

--- openblas/build.sh
+++ openblas/build.sh
@@ -3,3 +3,8 @@
 make NO_AFFINITY=1 NUM_THREADS=1 DYNAMIC_ARCH=1 NO_STATIC=1 PREFIX=$PREFIX -j${CPU_COUNT}
 make install PREFIX=$PREFIX
 
+ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/libblas.a
+ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/liblapack.a
+
+ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/libblas.$DYLIB_EXT
+ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/liblapack.$DYLIB_EXT
