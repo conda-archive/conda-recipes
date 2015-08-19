0ad3d5e4afd27a09097956af5c206f425519575f
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-19 12:39:07 -0400

    pylibtiff/build.sh: Drop hacky `sed` style patch as the link appears to work.

--- pylibtiff/build.sh
+++ pylibtiff/build.sh
@@ -10,9 +10,5 @@ fi
 
 
 python setup.py install --prefix=$PREFIX
-ESC_PREFIX=$(python -c "print(\"${PREFIX}\".replace(\"/\", \"\\/\"))")
-NEWLINE=$'\n'
-sed -i.bak "s/if os.name=='nt':/os.environ[\"LD_LIBRARY_PATH\"] = \"${ESC_PREFIX}\/lib:\" + os.environ.get(\"LD_LIBRARY_PATH\", \"\")\\${NEWLINE}os.environ[\"DYLD_LIBRARY_PATH\"] = \"${ESC_PREFIX}\/lib:\" + os.environ.get(\"DYLD_LIBRARY_PATH\", \"\")\\${NEWLINE}if os.name=='nt':/g" $PREFIX/lib/python2.7/site-packages/libtiff/libtiff_ctypes.py
-rm $PREFIX/lib/python2.7/site-packages/libtiff/libtiff_ctypes.py.bak
 
 ln -s $PREFIX/lib/libtiff.$DYLIB_EXT $SP_DIR/libtiff/libtiff.$DYLIB_EXT
