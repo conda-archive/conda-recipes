d53cfbd82b5531b66093ee25e971dc1bdb9b30a4
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-19 12:29:17 -0400

    pylibtiff/build.sh: Link the tiff library into the package.

--- pylibtiff/build.sh
+++ pylibtiff/build.sh
@@ -14,3 +14,5 @@ ESC_PREFIX=$(python -c "print(\"${PREFIX}\".replace(\"/\", \"\\/\"))")
 NEWLINE=$'\n'
 sed -i.bak "s/if os.name=='nt':/os.environ[\"LD_LIBRARY_PATH\"] = \"${ESC_PREFIX}\/lib:\" + os.environ.get(\"LD_LIBRARY_PATH\", \"\")\\${NEWLINE}os.environ[\"DYLD_LIBRARY_PATH\"] = \"${ESC_PREFIX}\/lib:\" + os.environ.get(\"DYLD_LIBRARY_PATH\", \"\")\\${NEWLINE}if os.name=='nt':/g" $PREFIX/lib/python2.7/site-packages/libtiff/libtiff_ctypes.py
 rm $PREFIX/lib/python2.7/site-packages/libtiff/libtiff_ctypes.py.bak
+
+ln -s $PREFIX/lib/libtiff.$DYLIB_EXT $SP_DIR/libtiff/libtiff.$DYLIB_EXT
