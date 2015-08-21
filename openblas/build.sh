accbb990cea17b9be711b7bf95095c20e77552f3
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-21 11:50:23 -0400

    openblas/build.sh: Drop newline.

--- openblas/build.sh
+++ openblas/build.sh
@@ -20,6 +20,5 @@ fi
 
 ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/libblas.a
 ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/liblapack.a
-
 ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/libblas.$DYLIB_EXT
 ln -fs $PREFIX/lib/libopenblas.$DYLIB_EXT $PREFIX/lib/liblapack.$DYLIB_EXT
