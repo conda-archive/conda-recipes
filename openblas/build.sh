a44c16461c7a6974ebbb375f63b51b0c37f53725
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-21 11:47:35 -0400

    openblas/build.sh: Patch paths related to `gfortran` on Mac to make sure they refer to an appropriate library on the path.

--- openblas/build.sh
+++ openblas/build.sh
@@ -9,6 +9,15 @@ fi
 make DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 NUM_THREADS=1 -j${CPU_COUNT}
 make install PREFIX=$PREFIX
 
+if [[ `uname` == 'Darwin' ]]; then
+    GFORTRAN_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgfortran.3.dylib)\"))")
+    GCC_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgcc_s.1.dylib)\"))")
+    QUADMATH_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libquadmath.0.dylib)\"))")
+    install_name_tool -change $GFORTRAN_LIB @rpath/libgfortran.3.dylib $PREFIX/lib/libopenblas.dylib
+    install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $PREFIX/lib/libopenblas.dylib
+    install_name_tool -change $QUADMATH_LIB @rpath/libquadmath.0.dylib $PREFIX/lib/libopenblas.dylib
+fi
+
 ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/libblas.a
 ln -fs $PREFIX/lib/libopenblas.a $PREFIX/lib/liblapack.a
 
