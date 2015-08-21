cd9b6054d0249fe9e3a005e0dfdd190f8728aac6
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-08-21 12:10:12 -0400

    scipy-openblas/build.sh: On Mac, patch libs so any gfortran libs will be searched for on the path.

--- scipy-openblas/build.sh
+++ scipy-openblas/build.sh
@@ -3,3 +3,18 @@
 unset LDFLAGS
 
 $PYTHON setup.py install
+
+
+# Make sure the linked gfortran libraries are searched for on the RPATH.
+if [[ `uname` == 'Darwin' ]]; then
+    GFORTRAN_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgfortran.3.dylib)\"))")
+    GCC_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libgcc_s.1.dylib)\"))")
+    QUADMATH_LIB=$(python -c "from os.path import realpath; print(realpath(\"$(gfortran -print-file-name=libquadmath.0.dylib)\"))")
+
+    for EACH_FILE in `find ${SP_DIR}/scipy -name "*.so"`;
+    do
+        install_name_tool -change $GFORTRAN_LIB @rpath/libgfortran.3.dylib $EACH_FILE
+        install_name_tool -change $GCC_LIB @rpath/libgcc_s.1.dylib $EACH_FILE
+        install_name_tool -change $QUADMATH_LIB @rpath/libquadmath.0.dylib $EACH_FILE
+    done
+fi
