2b090fa8ef058f0ba79c30962a04d534f98e4878
Vladyslav Frolov <frolvlad@salford-systems.com>
2015-08-27 08:18:44 -0700

    invoke: new package

---
+++ invoke/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# conda-build tries to compile .pyc files in Python 2 environment, but Invoke
+# has yaml for Python3 codebase, which fails on Python2.
+# Related question on conda-build issue tracker:
+# https://github.com/conda/conda-build/pull/317#commitcomment-12926020
+if [[ "${PY_VER%%.*}" == "2" ]]; then
+    rm -rf invoke/vendor/yaml3
+fi
+
+$PYTHON setup.py install
