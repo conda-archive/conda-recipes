c6ac9ed44ed24189f2c48a9dccf9da8fb5584b53
Vladyslav Frolov <frolvlad@salford-systems.com>
2015-08-28 02:46:01 -0700

    invoke: Disabled noarch_python due to the bug in Conda: https://github.com/conda/conda/issues/1391

--- invoke/bld.bat
+++ invoke/bld.bat
@@ -2,6 +2,7 @@
 :: has yaml for Python3 codebase, which fails on Python2.
 :: Related question on conda-build issue tracker:
 :: https://github.com/conda/conda-build/pull/317#commitcomment-12926020
+:: WARNING: this has to be removed when noarch_python is enabled!
 if "%PY_VER%" == "2.7" rmdir /S /Q "invoke\vendor\yaml3"
 
 "%PYTHON%" setup.py install
--- invoke/build.sh
+++ invoke/build.sh
@@ -4,6 +4,7 @@
 # has yaml for Python3 codebase, which fails on Python2.
 # Related question on conda-build issue tracker:
 # https://github.com/conda/conda-build/pull/317#commitcomment-12926020
+# WARNING: this has to be removed when noarch_python is enabled!
 if [[ "${PY_VER%%.*}" == "2" ]]; then
     rm -rf invoke/vendor/yaml3
 fi
