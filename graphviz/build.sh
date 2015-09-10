4a3ef165eee3f7cecded8c3170415f643cb1ae8d
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-09-10 08:52:09 -0400

    graphviz/build.sh: Allow the architecture to be specified.

--- graphviz/build.sh
+++ graphviz/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # see http://conda.pydata.org/docs/build.html for hacking instructions.
 
-export CFLAGS="-Wall -g -m64 -pipe -O2 -fPIC"
+export CFLAGS="-Wall -g -m${ARCH} -pipe -O2 -fPIC"
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
