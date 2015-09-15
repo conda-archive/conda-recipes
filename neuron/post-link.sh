56da1f91551d0c9531d1ff0b44e0584b16037db7
Michele Mattioni <mattions@gmail.com>
2015-09-15 09:44:22 +0100

    Linking the proper filename

--- neuron/post-link.sh
+++ neuron/post-link.sh
@@ -1,5 +1,9 @@
 for f in ${PREFIX}/x86_64/lib/*; do
-    if [ ! -f $f ]; then
-     ln -sv ${PREFIX}/x86_64/lib/$f ${PREFIX}/lib/
+    f_basename="${f##*/}"
+    f_dest=${PREFIX}/lib/$f_basename
+    echo "Analyzing file $f_dest" >> $PREFIX/.messages.txt
+    if [ ! -f $f_dest ]; then
+        echo "$f_dest does not exist. Linking" >> $PREFIX/.messages.txt
+        ln -sv $f $f_dest
     fi
 done
