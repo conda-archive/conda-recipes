1f8b7036c8c0b330312ad49f52019f72938de965
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2015-09-18 16:00:57 +0200

    Improved miktex-portable build script

--- miktex-portable/bld.bat
+++ miktex-portable/bld.bat
@@ -4,5 +4,5 @@ if errorlevel 1 exit 1
 "%SCRIPTS%\7za" x .\miktex-portable-2.9.5719.exe -yo"%PREFIX%\miktex-portable\"
 if errorlevel 1 exit 1
 
-for %%f in ("%PREFIX%\miktex-portable\miktex\bin\*.exe") do echo %%f %%* >> "%SCRIPTS%\%%~nf.bat"
+for %%f in ("%PREFIX%\miktex-portable\miktex\bin\*.exe") do echo .\..\miktex-portable\miktex\bin\%%~nxf %%* >> "%SCRIPTS%\%%~nf.bat"
 if errorlevel 1 exit 1
