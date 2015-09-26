5b70498f25f5ff2aaf171a346a4ed36a1da93c03
Julien Schueller <schueller@phimeca.com>
2015-09-26 05:39:51 -0700

    Use LIBRARY_PREFIX

--- miktex/bld.bat
+++ miktex/bld.bat
@@ -1,9 +1,9 @@
-7za x miktex-portable-%PKG_VERSION%.exe -o%PREFIX%\share
+7za x miktex-portable-%PKG_VERSION%.exe -o%LIBRARY_PREFIX%
 
 rem latex tools must be run from miktex tree
 rem nbconvert uses pdflatex & bibtex
 rem SCRIPTS dir is created by 7za install
-@echo start /b /wait cmd /c ^"%%~dp0..\share\miktex\bin\pdflatex.exe %%*^" > %SCRIPTS%\pdflatex.bat
-@echo start /b /wait cmd /c ^"%%~dp0..\share\miktex\bin\bibtex.exe %%*^" > %SCRIPTS%\bibtex.bat
+@echo start /b /wait cmd /c ^"%%~dp0..\Library\miktex\bin\pdflatex.exe %%*^" > %SCRIPTS%\pdflatex.bat
+@echo start /b /wait cmd /c ^"%%~dp0..\Library\miktex\bin\bibtex.exe %%*^" > %SCRIPTS%\bibtex.bat
 
-for %%x in (adjustbox booktabs collectbox fancyvrb ifoddpage mptopdf ucs url) do %PREFIX%\share\miktex\bin\mpm --install %%x
+for %%x in (adjustbox booktabs collectbox fancyvrb ifoddpage mptopdf ucs url) do %LIBRARY_PREFIX%\miktex\bin\mpm --install %%x
