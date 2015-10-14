a9eb103d8a88cb9912815590903891f695ffd04a
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-14 16:34:49 -0500

    libffi: Add right includedir to its pc file

--- libffi/build.sh
+++ libffi/build.sh
@@ -5,8 +5,11 @@ make install || exit 1
 cd $PREFIX
 rm -rf share
 mv lib/libffi-*/include include
+
 if [ `uname -m` == x86_64 ]; then
     mv lib64/* lib/
     rmdir lib64
 fi
+
+sed -i s/'includedir=.*'/'includedir=\${exec_prefix}\/include'/g lib/pkgconfig/libffi.pc
 
