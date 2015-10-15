8cd3bc6a27b19588bda4fd5d1494834fc98dd429
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-15 00:04:24 -0500

    flex: Fix recipe because it was broken

--- flex/build.sh
+++ flex/build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# gstreamer expects libffi.la to be in lib64 for some reason.
-mkdir $PREFIX/lib64
-cp -r $LIBRARY_PATH/libffi* $PREFIX/lib64
-ls $PREFIX/lib64
-
-# The datarootdir option places the docs into a temp folder that won't
-# be included in the package (it is about 12MB).
-./configure --disable-examples --prefix="$PREFIX" --datarootdir=`pwd`/tmpshare
+./configure --prefix="$PREFIX"
 make
 make install
 
-# Remove the created lib64 directory
-rm -rf $PREFIX/lib64
