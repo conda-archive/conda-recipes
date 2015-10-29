0c3e068245c5c4a4e2aaec0e85bfc5182eb8fb5e
Lev Givon <lev@columbia.edu>
2015-10-29 10:43:31 -0400

    Use PREFIX instead of CONDA_ENV_PATH because the latter isn't defined when a pkg is installed during creation of a fresh conda env.

--- openmpi/post-link.sh
+++ openmpi/post-link.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-mkdir -p $CONDA_ENV_PATH/etc/conda/deactivate.d
-mkdir -p $CONDA_ENV_PATH/etc/conda/activate.d
-cat <<EOF > $CONDA_ENV_PATH/etc/conda/deactivate.d/openmpi.sh
+mkdir -p $PREFIX/etc/conda/deactivate.d
+mkdir -p $PREFIX/etc/conda/activate.d
+cat <<EOF > $PREFIX/etc/conda/deactivate.d/openmpi.sh
 echo unsetting OPAL_PREFIX
 export OPAL_PREFIX=$OPAL_PREFIX_OLD
 unset OPAL_PREFIX_OLD
 EOF
-cat <<EOF > $CONDA_ENV_PATH/etc/conda/activate.d/openmpi.sh
+cat <<EOF > $PREFIX/etc/conda/activate.d/openmpi.sh
 #!/bin/sh
-echo setting OPAL_PREFIX to $CONDA_ENV_PATH
+echo setting OPAL_PREFIX to $PREFIX
 export OPAL_PREFIX_OLD=$OPAL_PREFIX
-export OPAL_PREFIX=$CONDA_ENV_PATH
+export OPAL_PREFIX=$PREFIX
 EOF
 
--- openmpi/pre-unlink.sh
+++ openmpi/pre-unlink.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -f $CONDA_ENV_PATH/etc/conda/deactivate.d/openmpi.sh
-rm -f $CONDA_ENV_PATH/etc/conda/activate.d/openmpi.sh
+rm -f $PREFIX/etc/conda/deactivate.d/openmpi.sh
+rm -f $PREFIX/etc/conda/activate.d/openmpi.sh
