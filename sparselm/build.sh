5d9d4d6857c32182622b8101fa6244258611850f
Hannes Ovrén <hannes.ovren@liu.se>
2015-10-30 10:07:47 +0100

    sparselm: Build with -fPIC
    To later build pysparselm, we need to build libsplm.a
    with the -fPIC compiler flag.

--- sparselm/build.sh
+++ sparselm/build.sh
@@ -5,6 +5,7 @@ cmake                              \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DBUILD_DEMO=OFF               \
     -DCHOLMOD_INCDIR=/usr/include/suitesparse \
+    -DCMAKE_C_FLAGS="-fPIC" \
     ..
 make
 mkdir -p $PREFIX/lib64
