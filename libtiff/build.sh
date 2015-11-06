a09c6f6fb15a78704ec5165fd1b39d2e0dccd821
Stuart Berg <bergs@janelia.hhmi.org>
2015-11-06 13:51:00 -0500

    libtiff: Include jbig support

--- libtiff/build.sh
+++ libtiff/build.sh
@@ -9,16 +9,15 @@ TIFF_DOC="${PREFIX}/tiff-doc"
 mkdir "${TIFF_BIN}" "${TIFF_SHARE}" "${TIFF_DOC}"
 
 # Pass explicit paths to the prefix for each dependency.
-# Also, we don't support jbig, so disable it to avoid accidentally 
-# picking it up from the build machine (if it happens to be present).
 ./configure --prefix="${PREFIX}" \
             --with-zlib-include-dir="${PREFIX}/include" \
             --with-zlib-lib-dir="${PREFIX}/lib" \
             --with-jpeg-include-dir="${PREFIX}/include" \
             --with-jpeg-lib-dir="${PREFIX}/lib" \
+            --with-jbig-include-dir="${PREFIX}/include" \
+            --with-jbig-lib-dir="${PREFIX}/lib" \
             --with-lzma-include-dir="${PREFIX}/include" \
             --with-lzma-lib-dir="${PREFIX}/lib" \
-            --disable-jbig \
             --bindir="${TIFF_BIN}" \
             --datarootdir="${TIFF_SHARE}" \
             --docdir="${TIFF_DOC}" \
