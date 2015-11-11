03c4e6af3354649e877becde73380c6f58fdebe4
Carlos Cordoba <ccordoba12@gmail.com>
2015-11-11 11:39:35 -0500

    Pixman: Improve compilation on Windows

--- pixman/bld.bat
+++ pixman/bld.bat
@@ -1,9 +1,21 @@
-:: -- Comnpiling
-:: SSSE3 and MMX are giving erros on MSVC 9, so we are
-:: disabling them
-make -f Makefile.win32 SSSE3=off MMX=off
+:: SSSE3 is giving errors when compiling with MSVC 9
+if %PY_VER%==2.7 (
+    set SSSE3_FLAG=off
+) else (
+    set SSSE3_FLAG=on
+)
 
-:: -- Installing
+:: MMX is giving errors when linking cairo in 64 bit systems
+if %ARCH%==64 (
+    set MMX_FLAG=off
+) else (
+    set MMX_FLAG=on
+)
+
+:: Comnpiling
+make -f Makefile.win32 SSSE3=%SSSE3_FLAG% MMX=%MMX_FLAG%
+
+:: Installing
 mkdir %LIBRARY_INC%\pixman
 move pixman\pixman.h %LIBRARY_INC%\pixman
 move pixman\pixman-version.h %LIBRARY_INC%\pixman
