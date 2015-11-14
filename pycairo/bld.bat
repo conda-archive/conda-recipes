351bcd53a4bf54865dcfb8f751d86864817b038d
Carlos Cordoba <ccordoba12@gmail.com>
2015-11-14 01:27:37 -0500

    Pycairo: Use Gohlke's wheels for Python 3.5
    - This is because the conda-build compiled extension is not working.

--- pycairo/bld.bat
+++ pycairo/bld.bat
@@ -1,3 +1,12 @@
 copy %RECIPE_DIR%\setup_win.py %SRC_DIR%\setup.py
 
-python setup.py install
+:: Extension is not working on Python 3.5, so we're using Gohlke's wheels for now
+if %PY_VER%==3.5 (
+    if %ARCH%==64 (
+        pip install http://www.lfd.uci.edu/~gohlke/pythonlibs/xmshzit7/pycairo-1.10.0-cp35-none-win_amd64.whl
+    ) else (
+        pip install http://www.lfd.uci.edu/~gohlke/pythonlibs/xmshzit7/pycairo-1.10.0-cp35-none-win32.whl
+    )
+) else (
+    python setup.py install
+)
