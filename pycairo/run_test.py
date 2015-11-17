f55a3d1dee42847bee67e7c940e9d0d8fca475ea
Carlos Cordoba <ccordoba12@gmail.com>
2015-11-17 14:11:34 -0500

    Pycairo: Change run_test.py to test SVG generation

--- pycairo/run_test.py
+++ pycairo/run_test.py
@@ -1,32 +1,18 @@
-#!/usr/bin/env python
-"""/cairo-demo/cairo_snippets/gradient.cairo translated to Python
-"""
+# Taken from http://zetcode.com/gfx/pycairo/backends/
 
-import math
+import tempfile
+import os
+
 import cairo
+    
+ps = cairo.SVGSurface(os.path.join(tempfile.gettempdir(), "svgfile.svg"), 390, 60)
+cr = cairo.Context(ps)
 
-WIDTH, HEIGHT  = 256, 256
+cr.set_source_rgb(0, 0, 0)
+cr.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
+                    cairo.FONT_WEIGHT_NORMAL)
+cr.set_font_size(40)
 
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
-ctx = cairo.Context(surface)
-
-ctx.scale (WIDTH/1.0, HEIGHT/1.0)
-
-pat = cairo.LinearGradient (0.0, 0.0, 0.0, 1.0)
-pat.add_color_stop_rgba (1, 0, 0, 0, 1)
-pat.add_color_stop_rgba (0, 1, 1, 1, 1)
-
-ctx.rectangle (0,0,1,1)
-ctx.set_source (pat)
-ctx.fill ()
-
-pat = cairo.RadialGradient (0.45, 0.4, 0.1,
-                            0.4,  0.4, 0.5)
-pat.add_color_stop_rgba (0, 1, 1, 1, 1)
-pat.add_color_stop_rgba (1, 0, 0, 0, 1)
-
-ctx.set_source (pat)
-ctx.arc (0.5, 0.5, 0.3, 0, 2 * math.pi)
-ctx.fill ()
-
-surface.write_to_png('gradient.png')
+cr.move_to(10, 50)
+cr.show_text("Hello world!")
+cr.show_page()
