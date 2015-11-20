174ae4f5c70c9a2c2127387e925c818f48a7cb70
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2015-11-20 13:13:07 +0100

    Updated miktex-portable, scikit-image, azure and numpydoc recipes.

--- scikit-image/bld.bat
+++ scikit-image/bld.bat
@@ -1,10 +1,2 @@
 python setup.py install
 if errorlevel 1 exit 1
-
-del %SCRIPTS%\cython.pyc
-
-if "%PY3K%"=="1" (
-    rd /s /q %SP_DIR%\numpy
-)
-
-rd /s /q %SCRIPTS%
--- scikit-image/build.sh
+++ scikit-image/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 $PYTHON setup.py install
-
-rm -r $PREFIX/bin
