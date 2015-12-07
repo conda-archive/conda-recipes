930e9a0ecb9e711288b2a820d0df502d792135a4
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2015-12-07 17:14:25 +0100

    Added some explanatory comments

--- workhorse_compiled.bat
+++ workhorse_compiled.bat
@@ -1,26 +1,31 @@
-setlocal EnableDelayedExpansion
+@setlocal enableextensions enabledelayedexpansion
 
+REM configure according to your preferences
 set TARGET=clinicalgraphics
 set PKG=gdcm
 set VER=2.6.1
 set CONDA_BLD_DIR=%userprofile%\Miniconda\conda-bld
 set BUILD_NR=7
-
+REM configure these lists for the pythons and architectures you want to support
 set pythons=26_vc9 27_vc9 33_vc10 34_vc10 35_vc14
+set architectures=64 32
 
+REM try not to touch the rest of the code
 for %%p in (%pythons%) do (
     set python=%%p
     conda build .\%PKG% --python !python:~0,2! --no-anaconda-upload
 )
 
-call conda32
-
-for %%p in (%pythons%) do (
-    set python=%%p
-    conda build .\%PKG% --python !python:~0,2! --no-anaconda-upload
+if not %architectures:32=%==%architectures% (
+    REM conda32 should be a .bat script that prepends x86 conda.exe to the PATH
+    call conda32
+    for %%p in (%pythons%) do (
+        set python=%%p
+        conda build .\%PKG% --python !python:~0,2! --no-anaconda-upload
+    )
 )
 
-for %%b in (64 32) do (
+for %%b in (%architectures%) do (
     for %%p in (%pythons%) do (
         anaconda upload "%CONDA_BLD_DIR%\win-%%b\%PKG%-%VER%-py%%p_%BUILD_NR%.tar.bz2" -u %TARGET%
     )
--- workhorse_pure.bat
+++ workhorse_pure.bat
@@ -1,13 +1,15 @@
+REM configure according to your preferences
 set TARGET=clinicalgraphics
 set PKG=pydicom
 set VER=0.9.10
 set CONDA_BLD_DIR=%userprofile%\Miniconda\conda-bld
 set BUILD_PLATFORM=win-64
 set BUILD_NR=0
-
+REM configure these lists for the pythons and platforms you want to support
 set pythons=26 27 33 34 35
 set platforms=win-32 win-64 linux-32 linux-64 osx-64
 
+REM try not to touch the rest of the code
 for %%p in (%pythons%) do (
     conda build .\%PKG% --python %%p --no-anaconda-upload
 )
