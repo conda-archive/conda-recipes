0099d373de012d77e57baf6168ad3fb403bf1fe2
Carlos Cordoba <ccordoba12@gmail.com>
2015-12-09 19:48:58 -0500

    Pandoc: Update Linux version
    - Also use binary official package to build the conda one

--- pandoc/build.sh
+++ pandoc/build.sh
@@ -1,22 +1,12 @@
+#!/bin/bash
+
 if [ `uname` == Linux ]; then
-
-    # This might be different in general. You can find the general location by
-    # running ghci and running
-    #   :m System.Directory
-    #   getAppUserDataDirectory "cabal"
-    # I couldn't figure out how to run this dynamically. ghci doesn't seem to have
-    # a -c switch like python.
-    #export PATH="$HOME/.cabal/bin:$PATH"
-    cabal install alex happy
-    cabal install --only-dependencies
-    cabal install hsb2hs  # a required build tool
-    cabal install --flags="embed_data_files" citeproc-hs
-    cabal configure --flags="embed_data_files"
-    cabal build
-    mkdir -p $PREFIX/bin
-    cp $SRC_DIR/dist/build/pandoc/pandoc $PREFIX/bin/pandoc
-
+    ar vx pandoc*.deb
+    tar -xzvf data.tar.gz
+    mkdir $PREFIX/bin
+    mv usr/bin/* $PREFIX/bin
 fi
+
 
 if [ `uname` == Darwin ]; then
     pkgutil --expand pandoc-$PKG_VERSION.pkg pandoc
