cb9d6873d25e0e36d5d9f2934dee62df9ccbf951
Julien Schueller <schueller@phimeca.com>
2015-12-11 15:26:35 +0100

    nlopt: build python bindings

--- nlopt/build.sh
+++ nlopt/build.sh
@@ -1,3 +1,3 @@
-./configure --prefix=$PREFIX --enable-shared --without-octave --without-matlab --without-guile --without-python
-make
+PYTHON=$PREFIX/bin/python${PY_VER} ./configure --prefix=$PREFIX --enable-shared --without-octave --without-matlab --without-guile
+make -j${CPU_COUNT}
 make install
