3259ff0b1164acc7b5ae3cba75955b263be6049f
Paul Tunison <paul.tunison@kitware.com>
2015-12-16 13:56:40 -0500

    Update flann build instructions

--- flann/bld.bat
+++ flann/bld.bat
@@ -1,13 +1,18 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %SRC_DIR%
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% -DBUILD_C_BINDINGS:BOOL=ON -DBUILD_PYTHON_BINDINGS:BOOL=ON -DBUILD_MATLAB_BINDINGS:BOOL=OFF %SRC_DIR%
 if errorlevel 1 exit 1
 
 REM Build C libraries and tools
 nmake
 if errorlevel 1 exit 1
 
-REM Install step
+REM C Install step
 nmake install
+if errorlevel 1 exit 1
+
+REM Python Install step
+cd %LIBRARY_PREFIX%\share\flann\python
+%PYTHON% setup.py install --prefix "%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
--- flann/build.sh
+++ flann/build.sh
@@ -3,7 +3,19 @@
 mkdir build
 cd build
 
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} ${SRC_DIR}
+# Make/Install C/C++ portion
+cmake -G "Unix Makefiles" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  -DBUILD_C_BINDINGS:BOOL=ON \
+  -DBUILD_PYTHON_BINDINGS:BOOL=ON \
+  -DBUILD_MATLAB_BINDINGS:BOOL=OFF \
+  ${SRC_DIR}
 
 make -j$CPU_COUNT
 make install
+
+# Install python components via generated setup.py
+cd ${PREFIX}/share/flann/python
+${PYTHON} setup.py install --prefix "${PREFIX}"
