5bdf1053606be87cf57510a6adc27507e0d0a8fd
Michael Sarahan <msarahan@gmail.com>
2015-12-18 18:55:40 -0600

    add Kitware python files to libsvm build

--- libsvm-kitware/build.sh
+++ libsvm-kitware/build.sh
@@ -3,5 +3,12 @@
 cd TPL/libsvm-3.1-custom
 
 make lib
-mkdir $PREFIX/lib
+mkdir -p $PREFIX/lib
 mv libsvm* $PREFIX/lib/
+
+mkdir -p $SP_DIR
+mv python/svm.py $SP_DIR/
+mv python/svmutil.py $SP_DIR/
+
+mkdir -p $PREFIX/include
+mv svm.h ${PREFIX}/include/
