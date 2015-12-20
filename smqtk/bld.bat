2d1b8266976ad3421a10181656d95627542e1e97
Michael Sarahan <msarahan@gmail.com>
2015-12-20 12:13:45 -0600

    break up long lines.  Add tests.

--- smqtk/bld.bat
+++ smqtk/bld.bat
@@ -1,7 +1,12 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %SRC_DIR%
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+      %SRC_DIR%
+
 if errorlevel 1 exit 1
 
 REM Build C libraries and tools
--- smqtk/build.sh
+++ smqtk/build.sh
@@ -3,7 +3,10 @@
 mkdir build
 cd build
 
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} ${SRC_DIR}
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+      ${SRC_DIR}
 
 make -j$CPU_COUNT
 make install
