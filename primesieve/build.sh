d6839ce14817d20f4c7864a5ef4941f901193a5d
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-01-05 10:13:48 -0500

    primesieve/build.sh: Drop cruft.

--- primesieve/build.sh
+++ primesieve/build.sh
@@ -2,9 +2,3 @@
 
 $PYTHON setup.py install
 py.test
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
