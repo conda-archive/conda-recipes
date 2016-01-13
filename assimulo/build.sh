e53e8c25a6eef6cc09cd287f56852df1ea1b4d1c
Kamil Kwiek (irritum) <kamil.kwiek@continuum.io>
2016-01-13 14:18:37 +0100

    Added recipe for assimulo-2.8 package

---
+++ assimulo/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Build dependencies:
+# - blas-devel
+
+if [[ ${ARCH} == '64' ]]; then
+    declare -r BLAS_LIB_PATH='/usr/lib64'
+else
+    declare -r BLAS_LIB_PATH='/usr/lib'
+fi
+
+${PYTHON} setup.py install \
+    --lapack-home=${PREFIX}/lib \
+    --blas-home=${BLAS_LIB_PATH} \
+    --sundials-home=${PREFIX}/lib || exit 1;
