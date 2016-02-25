3812bdb887b1496e3886da53373c83a02582632a
Ryan Grout <rgrout@continuum.io>
2016-02-25 02:40:07 -0600

    mirror r-packages for mro
    MRO doesn't have the distinction between r-base and r-recommended.  Therefore, all the recipes had to modified to not depend on r-base or anything in r-recommended.

---
+++ mro-packages/r-nloptr/bld.bat
@@ -0,0 +1,20 @@
+@rem Download the nlopt source
+python "%RECIPE_DIR%\download_nlopt.py"
+if errorlevel 1 exit 1
+
+tar -zxf nlopt-2.4.2.tar.gz
+if errorlevel 1 exit 1
+
+bash "%RECIPE_DIR%\install_windows.sh"
+if errorlevel 1 exit 1
+
+set NLOPT_HOME=%PREFIX:\=/%
+
+"%R%" CMD INSTALL --build .
+if errorlevel 1 exit 1
+
+@rem Add more build steps here, if they are necessary.
+
+@rem See
+@rem http://docs.continuum.io/conda/build.html
+@rem for a list of environment variables that are set during the build process.
---
+++ mro-packages/r-pki/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# R refuses to build packages that mark themselves as Priority: Recommended
+mv DESCRIPTION DESCRIPTION.old
+grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+
+# The openssl package isn't built with the @rpath logic on OS X. Instead of
+# rebuilding it, we'll just use this as a workaround.
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib:$(HOME)/lib:/usr/local/lib:/lib:/usr/lib
+
+$R CMD INSTALL --build .
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
