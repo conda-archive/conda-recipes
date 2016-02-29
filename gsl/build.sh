a0bd4ee784b87102f2a2d822efc11ef3ad0e8316
Ray Donnelly <mingw.android@gmail.com>
2016-02-29 22:34:20 +0000

    gsl (osx): Use clang to work around a GCC/clang assembler bad interaction

--- gsl/build.sh
+++ gsl/build.sh
@@ -1,3 +1,10 @@
+if [[ "$(uname)" == "Darwin" ]]; then
+  # CC=clang otherwise:
+  # error: ambiguous instructions require an explicit suffix (could be 'filds', or 'fildl')
+  # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66509
+  export CC=clang
+fi
+
 ./configure --prefix=$PREFIX
 
 make
