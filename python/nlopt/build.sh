5161c77235b7030528266d709ff5e1a1c1255c6d
Ray Donnelly <rdonnelly@0313-rdonnelly.local>
2016-02-29 19:15:05 +0000

    nlopt (osx): Use clang as CC
    Otherwise: Symbol not found: ___emutls_get_address

--- python/nlopt/build.sh
+++ python/nlopt/build.sh
@@ -1,3 +1,18 @@
-PYTHON=$PREFIX/bin/python${PY_VER} ./configure --prefix=$PREFIX --enable-shared --without-octave --without-matlab --without-guile
+#!/usr/bin/env sh
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  # CC=clang otherwise:
+  # ImportError: dlopen(.../envs/_test/lib/python3.5/site-packages/_nlopt.so, 2): Symbol not found: ___emutls_get_address
+  export CC=clang
+fi
+
+./configure          \
+  --prefix=${PREFIX} \
+  --enable-shared    \
+  --without-octave   \
+  --without-matlab   \
+  --without-guile
+
 make -j${CPU_COUNT}
+
 make install
