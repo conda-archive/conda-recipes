86f39127f66e7ce285af8cf51fd85a116bca4b67
Kale Franz <kfranz@continuum.io>
2016-03-16 16:28:35 -0500

    raml almost identical to pyyaml

--- python/raml/bld.bat
+++ python/raml/bld.bat
@@ -1,2 +1,9 @@
-%PYTHON% setup.py install
+set INCLUDE=%LIBRARY_INC%
+set LIBPATH=%LIBRARY_LIB%
+set LIB=%LIBRARY_LIB%
+
+python setup.py install
+if errorlevel 1 exit 1
+
+copy %LIBRARY_BIN%\yaml.dll %SP_DIR%\raml\ext\
 if errorlevel 1 exit 1
