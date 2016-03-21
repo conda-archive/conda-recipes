be9938619ca61b5e9171182efb35dda301b01641
Kamil Kwiek (irritum) <kamil.kwiek@continuum.io>
2016-03-21 10:12:32 +0100

    Adjusted recipe of pyOpt-1.2.0 package

--- python/pyOpt/bld.bat
+++ python/pyOpt/bld.bat
@@ -1,20 +1,2 @@
-swig.exe 2> NUL
-if %ERRORLEVEL%==9009 GOTO Swig_missing
-
-REM Copy any licenced source files the optimizers
-REM SET SNOPT_DIR="z:\snopt7"
-REM IF EXIST %SNOPT_DIR% ( xcopy %SNOPT_DIR%\src\* "%SRC_DIR%"\pyOpt\pySNOPT\source\* /S/Y/I/Q )
-
-%SYS_PYTHON% setup.py install --compiler=mingw32 --prefix=%PREFIX%
-
-for %%x in (libgfortran-3.dll libquadmath-0.dll) do (
-   copy %SYS_PREFIX%\Scripts\%%x %SP_DIR%\pyopt\
-   if errorlevel 1 exit 1
-)
-
-GOTO End
-
-:Swig_missing
-Echo "Swig.exe is missing in PATH"
-
-:End
+%PYTHON% setup.py install --compiler=mingw32
+if errorlevel 1 exit 1
