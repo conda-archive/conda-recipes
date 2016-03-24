a23eb1b54f1cb00997d35a2ed8ff939d06732af4
Kamil Kwiek (irritum) <kamil.kwiek@continuum.io>
2016-03-24 14:24:37 +0100

    Fixed recipe of assimulo-2.8 package

---
+++ assimulo/run_test.py
@@ -0,0 +1,6 @@
+from assimulo.examples.cvode_basic import run_example as run_example1
+from assimulo.examples.ida_basic_backward import run_example as run_example2
+
+run_example1(with_plots=False)
+run_example2(with_plots=False)
+
