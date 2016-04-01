4b804762a43ef32214f067aa404fdc502c8777d0
Pavol Juhas <pavol.juhas@gmail.com>
2016-04-01 16:37:33 -0400

    Remove broken symbolic link fix-up for wxpython.
    Symbolic link had invalid target.  As-installed symbolic
    link in PREFIX/bin/wx-config works right.

--- python/wxpython/build.sh
+++ python/wxpython/build.sh
@@ -53,12 +53,6 @@ LinuxInstallation() {
         --record installed_files.txt --prefix="${PREFIX}" || return 1;
     popd;
 
-    rm ${PREFIX}/bin/wx-config || return 1;
-
-    pushd ${PREFIX};
-    ln -vs ../lib/wx/config/inplace-gtk2-unicode-3.0 wx-config || return 1;
-    popd;
-
     return 0;
 }
 
