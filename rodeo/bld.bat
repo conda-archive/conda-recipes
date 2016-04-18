1f20dd45e6581aff54b2f605d7220c445f03ec06
Albert DeFusco <albert.defusco@me.com>
2016-04-18 14:18:33 -0400

    rodeo with Navigator app entry
    works on Windows and Mac

---
+++ rodeo/bld.bat
@@ -0,0 +1,11 @@
+set MENU_DIR=%PREFIX%\Menu
+mkdir %MENU_DIR%
+
+copy %RECIPE_DIR%\rodeo.ico %MENU_DIR%
+if errorlevel 1 exit 1
+
+copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\rodeo.json
+if errorlevel 1 exit 1
+
+%PYTHON% setup.py install
+if errorlevel 1 exit 1
