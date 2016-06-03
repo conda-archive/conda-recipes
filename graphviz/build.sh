64d4a1d34009da4623f722377137155af1c999ee
Stuart Berg <bergs@janelia.hhmi.org>
2016-06-02 22:19:11 -0400

    graphviz: Major changes: - Build from scratch, from the official git repo - Require libtool and expat (they seem to be necessary, the package is broken without them) - Don't use cairo on OS X, since it's an optional dependency and it isn't available in the defaults channel (for OS X). - Moved post-link code into build.sh

--- graphviz/build.sh
+++ graphviz/build.sh
@@ -6,11 +6,26 @@ export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
+./autogen.sh
+
 if [ `uname` == Darwin ]; then
-./configure --prefix=$PREFIX --with-quartz | tee configure.log 2>&1
+./configure --prefix=$PREFIX --with-quartz
 else
-./configure --prefix=$PREFIX | tee configure.log 2>&1
+./configure --prefix=$PREFIX
 fi
-make install | tee make.log 2>&1
+
+make -j${CPU_COUNT}
+
+if [[ $(uname) == Darwin ]]; then
+    # For some reason, libltdl.7.dylib can't be found without setting the rpath,
+    # and it even prevents 'make install' from working.
+    install_name_tool -add_rpath ${PREFIX}/lib cmd/dot/.libs/dot
+fi 
+
+make install
 
 # vim: set ai et nu:
+
+# Configure plugins
+# (Writes ${PREFIX}/lib/graphviz/config with available plugin information.)
+$PREFIX/bin/dot -c
--- graphviz/post-link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-$PREFIX/bin/dot -c
