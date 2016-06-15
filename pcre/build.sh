f6e849ee482858c6cd1c5949f058181341e73663
Ray Donnelly <mingw.android@gmail.com>
2016-06-15 03:00:45 +0100

    pcre: Update to 8.39 and add utf-8 support

--- pcre/build.sh
+++ pcre/build.sh
@@ -7,7 +7,7 @@ export LDFLAGS="-L${PREFIX}/lib"
 
 mkdir -p ${PREFIX}/bin || exit 1;
 
-./configure --prefix="${PREFIX}" || return 1;
+./configure --prefix="${PREFIX}" --enable-utf || return 1;
 make || return 1;
 make install || return 1;
 
