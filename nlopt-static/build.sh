d51b9d4a0f79fc5b6bc66ff9639427111e836c86
Ray Donnelly <mingw.android@gmail.com>
2016-06-17 21:24:22 +0000

    nlopt-static: Hack around a build issue

--- nlopt-static/build.sh
+++ nlopt-static/build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [[ $(uname -s) == Linux ]]; then
+  export CPPFLAGS=$CPPFLAGS" -fPIC"
+  # I have no idea why I'm having to do this. libtool issue?
+  mkdir -p {stogo,util,mma,direct,praxis,cobyla,api,test,auglag,cdirect,newuoa,bobyqa,neldermead,luksan,slsqp,octave,crs,swig,mlsl,isres,esch}/mc/lib
+  mkdir -p mc/lib
+fi
+
 ./configure          \
   --prefix=${PREFIX} \
   --enable-static    \
