db8ca6314ecf6c9c61f904fe7176de3ee4cec91c
Ray Donnelly <mingw.android@gmail.com>
2016-06-24 01:32:52 +0100

    ncurses: Several fixes
    1. Build as utf-8 for nano (and anything modern).
    2. Add pkg-config files for nano and r-base.
    3. Create linker libraries for non-wide versions.
       They point directly to the wide libraries but
       that's completely fine with utf-8. For r-base
       though I'll submit a PR to look for ncursesw.
    4. Don't build terminfo library since it breaks
       everything (Python, nano, r-base), printing:
       "Name collision between $TERM and $TERM"
       .. forever.

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-mkdir $PREFIX/lib
-
-sh ./configure --prefix=$PREFIX \
+sh ./configure --prefix=${PREFIX} \
     --without-debug --without-ada --without-manpages \
     --with-shared --disable-overwrite --enable-termcap \
-    --with-termlib
-
+    --enable-pc-files --enable-widec \
+    --with-pkg-config-libdir=${PREFIX}/pc
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
+
+for _LIB in ncurses ncurses++ form panel menu; do
+  echo "INPUT(-l${_LIB}w)" > ${PREFIX}/lib/lib${_LIB}.so
+  ln -s ${_LIB}w.pc ${PREFIX}/lib/pkgconfig/${_LIB}.pc
+done
