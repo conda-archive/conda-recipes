d7d52b68edeccbf1c05cf36dd5f0a3e72afb7057
Ray Donnelly <mingw.android@gmail.com>
2016-07-13 13:11:49 +0100

    ncurses: Some more fixes
      * --enable-symlinks
        Fixes https://github.com/conda/conda/issues/303
      * OS X GCC doesn't grok `--no-cpp-precomp`
      * Add summary, description, doc_url and dev_url

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-sh ./configure --prefix=${PREFIX} \
-    --without-debug --without-ada --without-manpages \
-    --with-shared --disable-overwrite --enable-termcap \
-    --enable-pc-files --enable-widec \
-    --with-pkg-config-libdir=${PREFIX}/pc
-
-if [[ $(uname -s) == Darwin ]]; then
-    export CC=clang
-    export CXX=clang++
-fi
+./configure --prefix=${PREFIX} \
+            --without-debug --without-ada --without-manpages \
+            --with-shared --disable-overwrite --enable-termcap \
+            --enable-pc-files --enable-widec --enable-symlinks \
+            --with-pkg-config-libdir=${PREFIX}/pc
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
 
-for _LIB in ncurses ncurses++ form panel menu; do
-  echo "INPUT(-l${_LIB}w)" > ${PREFIX}/lib/lib${_LIB}.so
-  ln -s ${_LIB}w.pc ${PREFIX}/lib/pkgconfig/${_LIB}.pc
-done
+if [[ $(uname -s) == Linux ]]; then
+  _SOEXT=.so
+else
+  _SOEXT=.dylib
+fi
+
+pushd ${PREFIX}/lib
+  for _LIB in ncurses ncurses++ form panel menu; do
+    if [[ -f lib${_LIB}w${_SOEXT} ]]; then
+      ln -s lib${_LIB}w${_SOEXT} lib${_LIB}${_SOEXT}
+    fi
+    if [[ -f lib${_LIB}w.a ]]; then
+      ln -s lib${_LIB}w.a lib${_LIB}.a
+    fi
+    if [[ -f pkgconfig/${_LIB}w.pc ]]; then
+      ln -s ${_LIB}w.pc pkgconfig/${_LIB}.pc
+    fi
+  done
+popd
