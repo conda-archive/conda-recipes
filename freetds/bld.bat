8d14c40f6072454ea58b1a70ced6b931d977ffef
Ray Donnelly <mingw.android@gmail.com>
2016-08-31 11:30:43 +0100

    freetds: Added (1.00.9)

---
+++ freetds/bld.bat
@@ -0,0 +1,14 @@
+set CMAKE_GENERATOR=NMake Makefiles JOM
+mkdir build
+cd build
+
+cmake -G "%CMAKE_GENERATOR%" ^
+         -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+         -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         -DCMAKE_BUILD_TYPE=Release ^
+         -DBUILD_SHARED_LIBS=True ..
+if errorlevel 1 exit 1
+
+cmake --build . --config Release --target all
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
