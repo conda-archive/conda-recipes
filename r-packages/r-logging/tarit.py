5ac9e1c76036d0de156022f764a060380f79b46d
Ray Donnelly <mingw.android@gmail.com>
2016-12-06 13:08:22 +0000

    r: Updates for 3.3.2

--- r-packages/r-logging/tarit.py
+++ r-packages/r-logging/tarit.py
@@ -2,7 +2,7 @@ import tarfile
 import os
 
 print 'creating archive'
-out = tarfile.open('linux-64_r-logging-0.7_103-r3.3.1_3.tar.bz2', mode='w:bz2')
+out = tarfile.open('linux-64_r-logging-0.7_103-r3.3.2_0.tar.bz2', mode='w:bz2')
 fl = []
 for root, dirs, files in os.walk("logging"):
     for file in files:
@@ -20,6 +20,6 @@ out.close()
 print
 print 'Contents:'
 os.chdir('..')
-t = tarfile.open('linux-64_r-logging-0.7_103-r3.3.1_3.tar.bz2', 'r')
+t = tarfile.open('linux-64_r-logging-0.7_103-r3.3.2_0.tar.bz2', 'r')
 for member_info in t.getmembers():
     print member_info.name
