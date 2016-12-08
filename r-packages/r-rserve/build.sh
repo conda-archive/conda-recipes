cadd663bc1eccf7942fa91a1037836ae25a0b920
Ray Donnelly <mingw.android@gmail.com>
2016-12-08 16:56:53 +0000

    r-rserve: Add libiconv dependency
    .. and set PKG_LIBS to ${CONDA_PREFIX}/lib so that it is found.
    
    The reason for this is that rserve makes a standalone executable
    (or two; a client and a server).

--- r-packages/r-rserve/build.sh
+++ r-packages/r-rserve/build.sh
@@ -4,7 +4,10 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
-$R CMD INSTALL --build .
+# This package makes a standalone binary and needs to find libraries essential to R.
+# It is likely that src/Makevars.in should use $(MAIN_LINK) from lib/R/etc/Makeconf
+# for this instead of having to pass it in via the PKG_LIBS environment variable.
+PKG_LIBS="-L${PREFIX}/lib" $R CMD INSTALL --build .
 
 # Add more build steps here, if they are necessary.
 
