b8a03e336c548c18d97726d7ab76a2987eea2a1f
Ray Donnelly <mingw.android@gmail.com>
2016-12-09 00:04:03 +0000

    r-rserve: Fix Linux build
    The things I have to do sometimes ..

--- r-packages/r-rserve/build.sh
+++ r-packages/r-rserve/build.sh
@@ -7,7 +7,14 @@ grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 # This package makes a standalone binary and needs to find libraries essential to R.
 # It is likely that src/Makevars.in should use $(MAIN_LINK) from lib/R/etc/Makeconf
 # for this instead of having to pass it in via the PKG_LIBS environment variable.
-PKG_LIBS="-L${PREFIX}/lib" $R CMD INSTALL --build .
+if [[ $(uname) == Darwin ]]; then
+  PKG_LIBS="-L${PREFIX}/lib" $R CMD INSTALL --build .
+else
+  # .. on Linux the situation is even worse:
+  LD_LIBRARY_PATH=${PREFIX}/lib/R/lib:${PREFIX}/lib \
+  PKG_LIBS="-L${PREFIX}/lib -L${PREFIX}/lib/R/lib -lR -lRblas" \
+    $R CMD INSTALL --build .
+fi
 
 # Add more build steps here, if they are necessary.
 
