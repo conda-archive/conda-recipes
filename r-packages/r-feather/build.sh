2be863d702c9836f17dee32e97812f02d5ccd080
Ray Donnelly <mingw.android@gmail.com>
2017-01-26 23:45:47 +0000

    r-feather: Force MACOSX_DEPLOYMENT_TARGET=10.9
    For the c++11s

--- r-packages/r-feather/build.sh
+++ r-packages/r-feather/build.sh
@@ -4,6 +4,11 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
+# conda-build sets this to 10.7 at present, and that changes
+# the behaviour of clang++ such that <cstdint> is not found.
+if [[ ${MACOSX_DEPLOYMENT_TARGET} == 10.7 ]]; then
+  export MACOSX_DEPLOYMENT_TARGET=10.9
+fi
 $R CMD INSTALL --build .
 
 # Add more build steps here, if they are necessary.
