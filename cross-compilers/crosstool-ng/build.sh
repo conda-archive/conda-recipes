f10bec40bee9419de8542c33e51ebcdcce83a052
Ray Donnelly <mingw.android@gmail.com>
2017-02-28 15:53:10 +0000

    crosstool-ng: Add -Wl,-rpath,${PREFIX}/lib

--- cross-compilers/crosstool-ng/build.sh
+++ cross-compilers/crosstool-ng/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 export EXTRA_CFLAGS="-I${PREFIX}/include"
-export EXTRA_LDFLAGS="-L${PREFIX}/lib -lncursesw"
+export EXTRA_LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lncursesw"
 if [[ $(uname) == Darwin ]]; then
   export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 fi
