7b76034bcea9c59c268afc462ed26fe84ee1c978
Ray Donnelly <mingw.android@gmail.com>
2017-06-03 02:37:59 +0100

    more fortran fixes

--- cross-compilers/compilers_generic/install-libgfortran.sh
+++ cross-compilers/compilers_generic/install-libgfortran.sh
@@ -9,11 +9,7 @@ export PATH=${SRC_DIR}/gcc_built/bin:${SRC_DIR}/.build/${CHOST}/buildtools/bin:$
 mkdir -p $PREFIX/lib
 rm -f $PREFIX/lib/libgfortran* || true
 
-cp ${SRC_DIR}/gcc_built/$CHOST/sysroot/lib/libgfortran.so.3.0.0 $PREFIX/lib
-pushd $PREFIX/lib
-  ln -s libgfortran.so.3.0.0 libgfortran.so.3.0
-  ln -s libgfortran.so.3.0 libgfortran.so
-popd
+cp -f ${SRC_DIR}/gcc_built/$CHOST/sysroot/lib/libgfortran.so* $PREFIX/lib
 
 # Install Runtime Library Exception
 install -Dm644 $SRC_DIR/.build/src/gcc-${PKG_VERSION}/COPYING.RUNTIME \
