59e0d5fa1234b20c2b88aff9456d697457115311
Ray Donnelly <mingw.android@gmail.com>
2017-06-14 13:46:01 +0100

    r-packages: Updates one month after 3.4
    Note, this does not add any new dependencies. That will follow.

--- r-packages/r-cairo/build.sh
+++ r-packages/r-cairo/build.sh
@@ -4,18 +4,6 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
-# Since `pkg-config --exists cairo --debug`
-# fails because we have no xrender.pc, this
-# is not such a big deal as our cairo is a
-# shared library and xrender.pc is used only
-# when linking statically. Still not ideal.
-# perhaps a dummy xrender.pc could be added
-# to the cairo package?
-if [[ $(uname -s) == Linux ]]; then
-  export CAIRO_LIBS="-L${PREFIX}/lib -lcairo"
-  export CAIRO_CFLAGS=-I${PREFIX}/include/cairo
-fi
-
 $R CMD INSTALL --build .
 
 # Add more build steps here, if they are necessary.
--- r-packages/r-rjava/build.sh
+++ r-packages/r-rjava/build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-unset R_JAVA_LD_LIBRARY_PATH
-unset JAVA_HOME
-unset LD_LIBRARY_PATH
-# Because I neutered ldpaths to do nothing when $(uname) is 'Linux'
-echo "!#/usr/bin/env bash" > ./uname
-echo "echo faked" >> ./uname
-chmod +x ./uname
-PATH=$PWD:$PATH . ${PREFIX}/lib/R/etc/ldpaths
-
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
