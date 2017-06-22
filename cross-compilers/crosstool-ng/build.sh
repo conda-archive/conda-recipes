05d27618046cfe1c3075deacd2940d11f4f4b0e3
Ray Donnelly <mingw.android@gmail.com>
2017-06-22 10:43:59 +0100

    crosstool-ng{_generic,}: More messing with {EXTRA_,}{C,CPP,LD}FLAGS
    This time so that configure and compilation works on systems without
    a system ncurses-devel package installed.

--- cross-compilers/crosstool-ng/build.sh
+++ cross-compilers/crosstool-ng/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export EXTRA_CFLAGS="-I${PREFIX}/include"
+export EXTRA_CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/ncurses"
 export EXTRA_LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lncursesw"
+export CPPFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 if [[ $(uname) == Darwin ]]; then
   export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 fi
 ./bootstrap
 ./configure --prefix=${PREFIX}
-make
+make V=1
 make install
