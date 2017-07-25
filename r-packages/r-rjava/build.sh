1547b31613423b095649d24fc6f6489bff262875
Ray Donnelly <mingw.android@gmail.com>
2017-07-25 17:26:59 -0500

    r-rjava: Set value for LD_LIBRARY_PATH/DYLD_FALLBACK_LIBRARY_PATH during build

--- r-packages/r-rjava/build.sh
+++ r-packages/r-rjava/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# All this (and it is fairly brittle) just to get the
+# value for JAVA_LD_LIBRARY_PATH as determined by:
+# R CMD javareconf
+echo 'echo $JAVA_LD_LIBRARY_PATH' > $PWD/java_reconf
+chmod +x $PWD/java_reconf
+JAVA_LD_LIBRARY_PATH=$($R CMD javareconf -e $PWD/java_reconf | tail -n1)
+echo "JAVA_LD_LIBRARY_PATH as determined by R CMD javareconf: $JAVA_LD_LIBRARY_PATH"
+
+if [[ $(uname) == Linux ]]; then
+  export LD_LIBRARY_PATH=${JAVA_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
+elif [[ $(uname) == Darwin ]]; then
+  export DYLD_FALLBACK_LIBRARY_PATH=${JAVA_LD_LIBRARY_PATH}:${DYLD_FALLBACK_LIBRARY_PATH}
+fi
+
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
