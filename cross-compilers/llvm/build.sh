a0df65c9263c477dd4d1137489542fc8c08ad275
Ray Donnelly <mingw.android@gmail.com>
2017-08-09 17:56:21 -0700

    llvm: Rebuild clang+llvm a 2nd time to flush out /usr/lib/libc++

--- cross-compilers/llvm/build.sh
+++ cross-compilers/llvm/build.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+pushd cctools
+  autoreconf -vfi
+  # Yuck, sorry.
+  [[ -d include/macho-o ]] || mkdir -p include/macho-o
+  cp ld64/src/other/prune_trie.h include/mach-o/prune_trie.h
+  cp ld64/src/other/prune_trie.h libprunetrie/prune_trie.h
+  cp ld64/src/other/PruneTrie.cpp libprunetrie/PruneTrie.cpp
+popd
+
 VERBOSE_CM="VERBOSE=1"
 VERBOSE_AT="V=1"
+
+# We need to be careful to avoid linking to any shared libs.
+# I have seen ncurses and zlib linked to the final binaries.
+find ${PREFIX}/lib -name "*${SHLIB_EXT}"
 
 # conda-build cleans out ${PREFIX} on each build. Also the sub-package install
 # scripts need to selectively copy things across to ${PREFIX} from ${PWD}/prefix
 # so we build to a different prefix and handle the copying manually. Because of
-# this we need to tell the compilers to look in ${PREFIX} for our dependencies.
+# this we need to tell the compilers to look in ${PREFIX} for our conda-package
+# dependencies.
 export LDFLAGS=${LDFLAGS}" -L${PREFIX}/lib"
 export CFLAGS=${CFLAGS}" -I${PREFIX}/include"
 export CXXFLAGS=${CXXFLAGS}" -I${PREFIX}/include"
+
 OLD_PREFIX=${PREFIX}
 PREFIX=${SRC_DIR}/prefix
 # We want to stop using the bootstrap compiler so will remove it from PATH
@@ -46,51 +61,15 @@ elif [[ $(uname) == Darwin ]]; then
   SYSROOT_DIR=${SRC_DIR}/bootstrap/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk
   CFLAG_SYSROOT="--sysroot ${SYSROOT_DIR}"
 
-  # Still having trouble with atomics in tsan:
-  # [ 27%] Building CXX object projects/compiler-rt/lib/tsan/CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interceptors_mac.cc.o
-  # cd /Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/llvm_build/projects/compiler-rt/lib/tsan && /Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/bootstrap/bin/clang++   -D_DEBUG -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -Dclang_rt_tsan_osx_dynamic_EXPORTS -I/Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/llvm_build/projects/compiler-rt/lib/tsan -I/Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/projects/compiler-rt/lib/tsan -I/Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/llvm_build/include -I/Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/include -I/Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/projects/compiler-rt/lib/tsan/..  -arch x86_64  -fPIC -fvisibility-inlines-hidden -Wall -W -Wno-unused-parameter -Wwrite-strings -Wcast-qual -Wmissing-field-initializers -pedantic -Wno-long-long -Wcovered-switch-default -Wnon-virtual-dtor -Wdelete-non-virtual-dtor -Wstring-conversion -Werror=date-time -std=c++11 -Wall -std=c++11 -Wno-unused-parameter -g -arch x86_64 -fPIC    -stdlib=libc++ -mmacosx-version-min=10.9 -isysroot / -fPIC -fno-builtin -fno-exceptions -fomit-frame-pointer -funwind-tables -fno-stack-protector -fno-sanitize=safe-stack -fvisibility=hidden -fvisibility-inlines-hidden -fno-function-sections -fno-lto -O3 -gline-tables-only -Wno-gnu -Wno-variadic-macros -Wno-c99-extensions -Wno-non-virtual-dtor -fPIE -fno-rtti -msse3 -Wframe-larger-than=530 -Wglobal-constructors -o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interceptors_mac.cc.o -c /Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/projects/compiler-rt/lib/tsan/rtl/tsan_interceptors_mac.cc
-  # /Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/bootstrap/bin/clang++   -arch x86_64  -fPIC -fvisibility-inlines-hidden -Wall -W -Wno-unused-parameter -Wwrite-strings -Wcast-qual -Wmissing-field-initializers -pedantic -Wno-long-long -Wcovered-switch-default -Wnon-virtual-dtor -Wdelete-non-virtual-dtor -Wstring-conversion -Werror=date-time -std=c++11 -Wall -std=c++11 -Wno-unused-parameter -g -arch x86_64 -dynamiclib -Wl,-headerpad_max_install_names  -stdlib=libc++ -lc++ -lc++abi -fapplication-extension -mmacosx-version-min=10.9 -isysroot / -Wl,-U,___ubsan_default_options -Wl,-U,___sanitizer_free_hook -Wl,-U,___sanitizer_malloc_hook -Wl,-U,___sanitizer_symbolize_code -Wl,-U,___sanitizer_symbolize_data -Wl,-U,___sanitizer_symbolize_demangle -Wl,-U,___sanitizer_symbolize_flush   -arch x86_64 -o ../../../../lib/clang/4.0.1/lib/darwin/libclang_rt.tsan_osx_dynamic.dylib -install_name @rpath/libclang_rt.tsan_osx_dynamic.dylib CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_clock.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_debugging.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_fd.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_flags.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_ignoreset.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interceptors.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interface.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interface_ann.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interface_atomic.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interface_java.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_malloc_mac.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_md5.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_mman.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_mutex.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_mutexset.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_preinit.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_report.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_rtl.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_rtl_mutex.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_rtl_proc.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_rtl_report.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_rtl_thread.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_stack_trace.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_stat.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_suppressions.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_symbolize.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_sync.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_interceptors_mac.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_libdispatch_mac.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_platform_mac.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_platform_posix.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_new_delete.cc.o CMakeFiles/clang_rt.tsan_osx_dynamic.dir/rtl/tsan_rtl_amd64.S.o ../interception/CMakeFiles/RTInterception.osx.dir/interception_linux.cc.o ../interception/CMakeFiles/RTInterception.osx.dir/interception_mac.cc.o ../interception/CMakeFiles/RTInterception.osx.dir/interception_win.cc.o ../interception/CMakeFiles/RTInterception.osx.dir/interception_type_test.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_allocator.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_common.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_deadlock_detector1.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_deadlock_detector2.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_flags.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_flag_parser.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_libc.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_libignore.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_linux.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_linux_s390.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_mac.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_persistent_allocator.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_platform_limits_linux.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_platform_limits_posix.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_posix.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_printf.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_procmaps_common.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_procmaps_freebsd.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_procmaps_linux.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_procmaps_mac.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_stackdepot.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_stacktrace.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_stacktrace_printer.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_suppressions.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_symbolizer.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_symbolizer_libbacktrace.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_symbolizer_mac.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_symbolizer_win.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_tls_get_addr.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_thread_registry.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_win.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommon.osx.dir/sanitizer_termination.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_common_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sancov_flags.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_coverage_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_coverage_libcdep_new.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_coverage_mapping_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_linux_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_posix_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_stacktrace_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_stoptheworld_linux_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_symbolizer_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_symbolizer_posix_libcdep.cc.o ../sanitizer_common/CMakeFiles/RTSanitizerCommonLibc.osx.dir/sanitizer_unwind_linux_libcdep.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_diag.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_init.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_flags.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_handlers.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_value.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_handlers_cxx.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_type_hash.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_type_hash_itanium.cc.o ../ubsan/CMakeFiles/RTUbsan.osx.dir/ubsan_type_hash_win.cc.o
-  # ld: warning: linking against dylib not safe for use in application extensions: /usr/lib/libc++.dylib
-  # ld: warning: linking against dylib not safe for use in application extensions: /usr/lib/libc++abi.dylib
-  # ld: warning: linking against dylib not safe for use in application extensions: /usr/lib/libSystem.dylib
-  # Undefined symbols for architecture x86_64:
-  #   "_OSAtomicDecrement32", referenced from:
-  #       _wrap_OSAtomicDecrement32 in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicDecrement32 in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicDecrement32Barrier, _wrap_OSAtomicDecrement32 )
-  #   "_OSAtomicDecrement32Barrier", referenced from:
-  #       _wrap_OSAtomicDecrement32Barrier in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicDecrement32Barrier in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicDecrement32Barrier)
-  #   "_OSAtomicDecrement64", referenced from:
-  #       _wrap_OSAtomicDecrement64 in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicDecrement64 in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicDecrement64, _wrap_OSAtomicDecrement64Barrier )
-  #   "_OSAtomicDecrement64Barrier", referenced from:
-  #       _wrap_OSAtomicDecrement64Barrier in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicDecrement64Barrier in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicDecrement64Barrier)
-  #   "_OSAtomicIncrement32", referenced from:
-  #       _wrap_OSAtomicIncrement32 in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicIncrement32 in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicIncrement32, _wrap_OSAtomicIncrement32Barrier )
-  #   "_OSAtomicIncrement32Barrier", referenced from:
-  #       _wrap_OSAtomicIncrement32Barrier in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicIncrement32Barrier in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicIncrement32Barrier)
-  #   "_OSAtomicIncrement64", referenced from:
-  #       _wrap_OSAtomicIncrement64 in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicIncrement64 in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicIncrement64, _wrap_OSAtomicIncrement64Barrier )
-  #   "_OSAtomicIncrement64Barrier", referenced from:
-  #       _wrap_OSAtomicIncrement64Barrier in tsan_interceptors_mac.cc.o
-  #       __tsan::substitution_OSAtomicIncrement64Barrier in tsan_interceptors_mac.cc.o
-  #      (maybe you meant: _wrap_OSAtomicIncrement64Barrier)
-  # ld: symbol(s) not found for architecture x86_64
 fi
 
 if [[ ${MACOSX_DEPLOYMENT_TARGET} == 10.9 ]]; then
   DARWIN_TARGET=x86_64-apple-darwin13.4.0
+fi
+
+if [[ -z ${DARWIN_TARGET} ]]; then
+  echo "Need a valid DARWIN_TARGET"
+  exit 1
 fi
 
 if [[ -n ${GCC} ]]; then
@@ -101,8 +80,6 @@ fi
 
 declare -a _cmake_config
 _cmake_config+=(-DCMAKE_INSTALL_PREFIX:PATH=${PREFIX})
-# TODO: how to add AArch64 here based on conda_build_config.yaml - does case matter?
-# _cmake_config+=(-DLLVM_TARGETS_TO_BUILD:STRING=X86;AArch64)
 _cmake_config+=(-DCMAKE_BUILD_TYPE:STRING=Release)
 # The bootstrap clang I use was built with a static libLLVMObject.a and I trying to get the same here
 # _cmake_config+=(-DBUILD_SHARED_LIBS:BOOL=ON)
@@ -140,15 +117,6 @@ _cctools_config+=(--build=$(${CC} -dumpmachine))
 _cctools_config+=(--target=${DARWIN_TARGET})
 _cctools_config+=(--prefix=${PREFIX})
 _cctools_config+=(--disable-static)
-
-pushd cctools
-  autoreconf -vfi
-    # Yuck, sorry.
-    [[ -d include/macho-o ]] || mkdir -p include/macho-o
-    cp ld64/src/other/prune_trie.h include/mach-o/prune_trie.h
-    cp ld64/src/other/prune_trie.h libprunetrie/prune_trie.h
-    cp ld64/src/other/PruneTrie.cpp libprunetrie/PruneTrie.cpp
-popd
 
 if [[ ! -f ${PREFIX}/bin/${DARWIN_TARGET}-ld ]]; then
 
@@ -202,24 +170,12 @@ if [[ ! -f ${PREFIX}/bin/${DARWIN_TARGET}-ld ]]; then
     if [[ ! -e ${PREFIX}/lib/libtapi${SHLIB_EXT} ]]; then
       [[ -d llvm_tapi_build ]] || mkdir llvm_tapi_build
       pushd llvm_tapi_build
-        # _cmake_config+=(-Wdev)
-        # _cmake_config+=(--debug-output)
-        # _cmake_config+=(--trace-expand)
         cmake -G'Unix Makefiles'                                 \
               -C ../projects/tapi/cmake/caches/apple-tapi.cmake  \
               "${_cmake_config[@]}"                              \
               -DCMAKE_LIBTOOL=${BOOTSTRAP}/bin/libtool           \
               ..
         make -j${CPU_COUNT} ${VERBOSE_CM} install-distribution
-        # tapi will fail to build here as it will not link to two libs, one of which has been built this time around (LLVMSupport)
-        # and one of which has not, LLVMObject (but it has been built by install-LTO, as a dylib whereas the official releases are
-        # static).
-        # To reproduce the failure:
-        # pushd /Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/llvm_tapi_build/projects/tapi/lib/Core
-        # /Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/bootstrap/bin/clang++  -mmacosx-version-min=10.9  -stdlib=libc++ -fPIC -fvisibility-inlines-hidden -Wall -W -Wno-unused-parameter -Wwrite-strings -Wcast-qual -Wmissing-field-initializers -pedantic -Wno-long-long -Wcovered-switch-default -Wnon-virtual-dtor -Wdelete-non-virtual-dtor -Wstring-conversion -Werror=date-time -std=c++11 -flto -Os    -DNDEBUG -isysroot /Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/bootstrap/MacOSX10.9.sdk -mmacosx-version-min=10.9 -dynamiclib -Wl,-headerpad_max_install_names  -Wl,-dead_strip -Wl,-object_path_lto,/Users/vagrant/conda/automated-build/bootstrap/mcf-x-build/cross-compiler/work/llvm_tapi_build/projects/tapi/lib/Core/./tapiCore-lto.o   -arch x86_64 -stdlib=libc++ -flto -o ../../../../lib/libtapiCore.dylib -install_name @rpath/libtapiCore.dylib CMakeFiles/tapiCore.dir/ArchitectureSupport.cpp.o CMakeFiles/tapiCore.dir/InterfaceFile.cpp.o CMakeFiles/tapiCore.dir/MachODylibReader.cpp.o CMakeFiles/tapiCore.dir/Registry.cpp.o CMakeFiles/tapiCore.dir/Symbol.cpp.o CMakeFiles/tapiCore.dir/TextStub_v1.cpp.o CMakeFiles/tapiCore.dir/TextStub_v2.cpp.o CMakeFiles/tapiCore.dir/YAMLReaderWriter.cpp.o -Wl,-rpath,@loader_path/../lib
-        # To work around it, add:
-        # -L../../../../lib/ -lLLVMSupport -L../../../../../llvm_lto_build/lib -lLLVMObject
-        # Overall, combining this in the same build as install-LTO might be wise? I am not sure if llvm-config would help with any of this too?
       popd
     fi
   fi
@@ -241,7 +197,7 @@ if [[ ! -f ${PREFIX}/bin/${DARWIN_TARGET}-ld ]]; then
   popd
 fi
 
-# Put our new cctools to the front of PATH, but keep bootstrap on.
+# Put our new cctools to the front of PATH, but also keep bootstrap.
 export PATH=${PREFIX}/bin:${PATH}
 
 if [[ ! -e "${SRC_DIR}/llvm_build/tools/clang/tools/c-index-test" ]]; then
@@ -274,6 +230,25 @@ if [[ ! -f ${cctools_build_final}/ld64/ld ]]; then
         --with-llvm=${PREFIX}                     \
         --disable-static
     make -j${CPU_COUNT} ${VERBOSE_AT}
+    make install
+  popd
+fi
+
+# Now we have built cctools with the new compilers rebuild clang
+# with them too. This is so that the libc++ they link to is not
+# from /usr/lib but instead relative to our build prefix.
+if [[ ! -e "${SRC_DIR}/llvm_build_final/tools/clang/tools/c-index-test" ]]; then
+  [[ -d llvm_build_final ]] || mkdir llvm_build_final
+  pushd llvm_build_final
+    CC=${PREFIX}/bin/clang" ${CFLAG_SYSROOT}"                                           \
+    CXX=${PREFIX}/bin/clang++" ${CFLAG_SYSROOT}"                                        \
+      cmake -G'Unix Makefiles'                                                          \
+            "${_cmake_config[@]}"                                                       \
+            -DCMAKE_LIBTOOL=${PREFIX}/bin/${DARWIN_TARGET}-libtool                      \
+            -DLD64_EXECUTABLE=${PREFIX}/bin/${DARWIN_TARGET}-ld                         \
+            -DCMAKE_INSTALL_NAME_TOOL=${PREFIX}/bin/${DARWIN_TARGET}-install_name_tool  \
+            ..
+    make -j${CPU_COUNT} ${VERBOSE_CM}
     make install
   popd
 fi
