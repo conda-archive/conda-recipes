13e4cccf2867f701521afabeda0a0a71dd84232d
Ray Donnelly <mingw.android@gmail.com>
2017-08-14 11:10:29 +0100

    Another go at that

--- cross-compilers/compilers_generic/install-libgcc.sh
+++ cross-compilers/compilers_generic/install-libgcc.sh
@@ -6,42 +6,45 @@ CHOST=$(${SRC_DIR}/.build/*-*-*-*/build/build-cc-gcc-final/gcc/xgcc -dumpmachine
 # .. do we need this scoped over the whole file though?
 export PATH=${SRC_DIR}/gcc_built/bin:${SRC_DIR}/.build/${CHOST}/buildtools/bin:${SRC_DIR}/.build/tools/bin:${PATH}
 
-pushd $SRC_DIR/.build/$CHOST/build/build-cc-gcc-final/
+pushd ${SRC_DIR}/.build/${CHOST}/build/build-cc-gcc-final/
 
-make -C $CHOST/libgcc prefix=${PREFIX} install-shared
+make -C ${CHOST}/libgcc prefix=${PREFIX} install-shared
 
 mkdir -p ${PREFIX}/${CHOST}/sysroot/lib || true
 for lib in libatomic libgomp libquadmath libitm libvtv libsanitizer/{a,l,ub,t}tsan; do
   if [[ -d ${CHOST}/${lib} ]]; then
     make -C ${CHOST}/${lib} prefix=${PREFIX} install-toolexeclibLTLIBRARIES
     make -C ${CHOST}/${lib} prefix=${PREFIX} install-nodist_fincludeHEADERS || true
-    symtargets=$(find ${PREFIX}/lib -name "${lib}.so*")
-    for symtarget in ${symtargets}; do
-      symtargetname=$(basename ${symtarget})
-      ln -s ${PREFIX}/lib/${symtargetname} ${PREFIX}/${CHOST}/sysroot/lib/${symtargetname}
-    done
   fi
 done
 
 
 for lib in libgomp libquadmath; do
-  if [[ -d $CHOST/$lib ]]; then
-    make -C $CHOST/$lib prefix=${PREFIX} install-info
+  if [[ -d ${CHOST}/${lib} ]]; then
+    make -C ${CHOST}/${lib} prefix=${PREFIX} install-info
   fi
 done
 
 popd
 
-mkdir -p $PREFIX/lib
-mv $PREFIX/$CHOST/lib/* $PREFIX/lib
+mkdir -p ${PREFIX}/lib
+mv ${PREFIX}/${CHOST}/lib/* ${PREFIX}/lib
+
+for lib in libatomic libgomp libquadmath libitm libvtv libsanitizer/{a,l,ub,t}tsan; do
+  symtargets=$(find ${PREFIX}/lib -name "${lib}.so*")
+  for symtarget in ${symtargets}; do
+    symtargetname=$(basename ${symtarget})
+    ln -s ${PREFIX}/lib/${symtargetname} ${PREFIX}/${CHOST}/sysroot/lib/${symtargetname}
+  done
+done
 
 # no static libs
-find $PREFIX/lib -name "*\.a" -exec rm -rf {} \;
+find ${PREFIX}/lib -name "*\.a" -exec rm -rf {} \;
 # no libtool files
-find $PREFIX/lib -name "*\.la" -exec rm -rf {} \;
+find ${PREFIX}/lib -name "*\.la" -exec rm -rf {} \;
 # clean up empty folder
-rm -rf $PREFIX/lib/gcc
+rm -rf ${PREFIX}/lib/gcc
 
 # Install Runtime Library Exception
-install -Dm644 $SRC_DIR/.build/src/gcc-${PKG_VERSION}/COPYING.RUNTIME \
+install -Dm644 ${SRC_DIR}/.build/src/gcc-${PKG_VERSION}/COPYING.RUNTIME \
         ${PREFIX}/share/licenses/gcc-libs/RUNTIME.LIBRARY.EXCEPTION
